160dda31f8ef7129160693c5a8ea12a9fe196fe2
Mzack9999 <marco.rivoli.nvh@gmail.com>
2020-04-26 23:32:58 +0200

    poc dsl language support in matchers

--- pkg/matchers/compile.go
+++ pkg/matchers/compile.go
@@ -1,8 +1,15 @@
 package matchers
 
 import (
+	"crypto/md5"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
 	"fmt"
 	"regexp"
+	"strings"
+
+	"github.com/Knetic/govaluate"
 )
 
 // CompileMatchers performs the initial setup operation on a matcher
@@ -25,6 +32,16 @@ func (m *Matcher) CompileMatchers() error {
 		m.regexCompiled = append(m.regexCompiled, compiled)
 	}
 
+	// Compile the dsl expressions
+	for _, dsl := range m.DSL {
+		compiled, err := govaluate.NewEvaluableExpressionWithFunctions(dsl, helperFunctions())
+		if err != nil {
+			return fmt.Errorf("could not compile dsl: %s", dsl)
+		}
+
+		m.dslCompiled = append(m.dslCompiled, compiled)
+	}
+
 	// Setup the condition type, if any.
 	if m.Condition != "" {
 		m.condition, ok = ConditionTypes[m.Condition]
@@ -45,4 +62,35 @@ func (m *Matcher) CompileMatchers() error {
 		m.part = BodyPart
 	}
 	return nil
+}
+
+func helperFunctions() (functions map[string]govaluate.ExpressionFunction) {
+	functions = make(map[string]govaluate.ExpressionFunction)
+	functions["len"] = func(args ...interface{}) (interface{}, error) {
+		length := len(args[0].(string))
+		return (float64)(length), nil
+	}
+	functions["base64"] = func(args ...interface{}) (interface{}, error) {
+		sEnc := base64.StdEncoding.EncodeToString([]byte(args[0].(string)))
+		return sEnc, nil
+	}
+	functions["md5"] = func(args ...interface{}) (interface{}, error) {
+		hash := md5.Sum([]byte(args[0].(string)))
+		return hex.EncodeToString(hash[:]), nil
+	}
+	functions["sha256"] = func(args ...interface{}) (interface{}, error) {
+		return sha256.Sum256([]byte(args[0].(string))), nil
+	}
+	functions["contains"] = func(args ...interface{}) (interface{}, error) {
+		return strings.Contains(args[0].(string), args[1].(string)), nil
+	}
+	functions["regex"] = func(args ...interface{}) (interface{}, error) {
+		compiled, err := regexp.Compile(args[0].(string))
+		if err != nil {
+			return nil, err
+		}
+		return compiled.MatchString(args[1].(string)), nil
+	}
+
+	return
 }
