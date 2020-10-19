6fb0e8dc761db603a63404d0c1f13d7deb5d217b
Ice3man543 <nizamulrana@gmail.com>
2020-10-19 11:42:12 +0530

    Removed comment

--- v2/pkg/executer/utils.go
+++ v2/pkg/executer/utils.go
@@ -9,22 +9,6 @@ import (
 
 type jsonOutput map[string]interface{}
 
-/*
-type jsonOutput struct {
-	Template         string                 `json:"template"`
-	Type             string                 `json:"type"`
-	Matched          string                 `json:"matched"`
-	MatcherName      string                 `json:"matcher_name,omitempty"`
-	ExtractedResults []string               `json:"extracted_results,omitempty"`
-	Name             string                 `json:"name"`
-	Severity         string                 `json:"severity"`
-	Author           string                 `json:"author"`
-	Description      string                 `json:"description"`
-	Request          string                 `json:"request,omitempty"`
-	Response         string                 `json:"response,omitempty"`
-	Meta             map[string]interface{} `json:"meta,omitempty"`
-}*/
-
 // unsafeToString converts byte slice to string with zero allocations
 func unsafeToString(bs []byte) string {
 	return *(*string)(unsafe.Pointer(&bs))
