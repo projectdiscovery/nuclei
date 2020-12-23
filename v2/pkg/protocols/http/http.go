dc2422818de9a96294f30d50d9188458e49c2848
Ice3man543 <nizamulrana@gmail.com>
2020-12-23 16:16:16 +0530

    Data modelling, work on executor started

--- v2/pkg/protocols/http/http.go
+++ v2/pkg/protocols/http/http.go
@@ -8,42 +8,17 @@ import (
 
 // Request contains a http request to be made from a template
 type Request struct {
-	// Path contains the path/s for the request
-	Path []string `yaml:"path"`
-	// Matchers contains the detection mechanism for the request to identify
-	// whether the request was successful
-	Matchers []*matchers.Matcher `yaml:"matchers,omitempty"`
-	// Extractors contains the extraction mechanism for the request to identify
-	// and extract parts of the response.
-	Extractors []*extractors.Extractor `yaml:"extractors,omitempty"`
-	// Raw contains raw requests
-	Raw  []string `yaml:"raw,omitempty"`
-	Name string   `yaml:"Name,omitempty"`
-	// AttackType is the attack type
-	// Sniper, PitchFork and ClusterBomb. Default is Sniper
-	AttackType string `yaml:"attack,omitempty"`
-	// Method is the request method, whether GET, POST, PUT, etc
-	Method string `yaml:"method"`
-	// Body is an optional parameter which contains the request body for POST methods, etc
-	Body string `yaml:"body,omitempty"`
-	// MatchersCondition is the condition of the matchers
-	// whether to use AND or OR. Default is OR.
-	MatchersCondition string `yaml:"matchers-condition,omitempty"`
-	// attackType is internal attack type
-	attackType generators.Type
-	// Path contains the path/s for the request variables
-	Payloads map[string]interface{} `yaml:"payloads,omitempty"`
-	// Headers contains headers to send with the request
-	Headers map[string]string `yaml:"headers,omitempty"`
-	// matchersCondition is internal condition for the matchers.
-	matchersCondition matchers.ConditionType
+	// Number of same request to send in race condition attack
+	RaceNumberRequests int `yaml:"race_count,omitempty"`
 	// MaxRedirects is the maximum number of redirects that should be followed.
 	MaxRedirects                  int `yaml:"max-redirects,omitempty"`
 	PipelineConcurrentConnections int `yaml:"pipeline-concurrent-connections,omitempty"`
 	PipelineRequestsPerConnection int `yaml:"pipeline-requests-per-connection,omitempty"`
 	Threads                       int `yaml:"threads,omitempty"`
-	// Internal Finite State Machine keeping track of scan process
-	gsfm *GeneratorFSM
+	// attackType is internal attack type
+	attackType generators.Type
+	// matchersCondition is internal condition for the matchers.
+	matchersCondition matchers.ConditionType
 	// CookieReuse is an optional setting that makes cookies shared within requests
 	CookieReuse bool `yaml:"cookie-reuse,omitempty"`
 	// Redirects specifies whether redirects should be followed.
@@ -60,6 +35,30 @@ type Request struct {
 	// Race determines if all the request have to be attempted at the same time
 	// The minimum number fof requests is determined by threads
 	Race bool `yaml:"race,omitempty"`
-	// Number of same request to send in race condition attack
-	RaceNumberRequests int `yaml:"race_count,omitempty"`
+	// Name is the name of the request
+	Name string `yaml:"Name,omitempty"`
+	// AttackType is the attack type
+	// Sniper, PitchFork and ClusterBomb. Default is Sniper
+	AttackType string `yaml:"attack,omitempty"`
+	// Method is the request method, whether GET, POST, PUT, etc
+	Method string `yaml:"method"`
+	// Body is an optional parameter which contains the request body for POST methods, etc
+	Body string `yaml:"body,omitempty"`
+	// MatchersCondition is the condition of the matchers
+	// whether to use AND or OR. Default is OR.
+	MatchersCondition string `yaml:"matchers-condition,omitempty"`
+	// Path contains the path/s for the request
+	Path []string `yaml:"path"`
+	// Raw contains raw requests
+	Raw []string `yaml:"raw,omitempty"`
+	// Matchers contains the detection mechanism for the request to identify
+	// whether the request was successful
+	Matchers []*matchers.Matcher `yaml:"matchers,omitempty"`
+	// Extractors contains the extraction mechanism for the request to identify
+	// and extract parts of the response.
+	Extractors []*extractors.Extractor `yaml:"extractors,omitempty"`
+	// Path contains the path/s for the request variables
+	Payloads map[string]interface{} `yaml:"payloads,omitempty"`
+	// Headers contains headers to send with the request
+	Headers map[string]string `yaml:"headers,omitempty"`
 }
--- v2/pkg/protocols/protocols.go
+++ v2/pkg/protocols/protocols.go
@@ -1,9 +1,23 @@
 package protocols
 
-// Rule is an interface implemented by a protocol rule
-type Rule interface {
-	// Compile compiles the protocol request for further execution.
+import "github.com/projectdiscovery/nuclei/v2/pkg/output"
+
+// RequestGenerator is an interface implemented by request generator for a protocol.
+type RequestGenerator interface {
+	// Next returns the next request in queue for the generator interface.
+	// If no requests are remaining, next returns io.EOF error.
+	Next() (interface{}, error)
+	// Compile compiles the request generators preparing any requests possible.
 	Compile() error
 	// Requests returns the total number of requests the rule will perform
 	Requests() int64
 }
+
+// Executer executes requests from a generator and returns an output event.
+type Executer interface {
+	// Execute executes the generator requests and returns an output event channel.
+	Execute(generator RequestGenerator, callback OutputEventCallback) error
+}
+
+// OutputEventCallback is a callback for each recieved output from executor
+type OutputEventCallback func(event output.Event)
