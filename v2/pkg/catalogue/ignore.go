fa5a3d472924b4eab4bcc121f29bcacef9a27a27
Ice3man543 <nizamulrana@gmail.com>
2021-01-14 12:22:19 +0530

    Better ignore functionality with ignore tests

--- v2/pkg/catalogue/ignore.go
+++ v2/pkg/catalogue/ignore.go
@@ -37,15 +37,6 @@ func (c *Catalogue) checkIfInNucleiIgnore(item string) bool {
 	}
 
 	for _, paths := range c.ignoreFiles {
-		// If we have a path to ignore, check if it's in the item.
-		if paths[len(paths)-1] == '/' {
-			if strings.Contains(item, paths) {
-				return true
-			}
-
-			continue
-		}
-		// Check for file based extension in ignores
 		if strings.HasSuffix(item, paths) {
 			return true
 		}
@@ -55,15 +46,18 @@ func (c *Catalogue) checkIfInNucleiIgnore(item string) bool {
 
 // ignoreFilesWithExcludes ignores results with exclude paths
 func (c *Catalogue) ignoreFilesWithExcludes(results, excluded []string) []string {
-	excludeMap := make(map[string]struct{}, len(excluded))
-	for _, excl := range excluded {
-		excludeMap[excl] = struct{}{}
-	}
+	var templates []string
 
-	templates := make([]string, 0, len(results))
-	for _, incl := range results {
-		if _, found := excludeMap[incl]; !found {
-			templates = append(templates, incl)
+	for _, result := range results {
+		matched := false
+		for _, paths := range excluded {
+			if strings.HasSuffix(result, paths) {
+				matched = true
+				break
+			}
+		}
+		if !matched {
+			templates = append(templates, result)
 		}
 	}
 	return templates
