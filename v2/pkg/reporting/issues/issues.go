4c7f4405b138085fd4971b089120cf3ebe8c87bd
Ice3man543 <nizamulrana@gmail.com>
2021-02-08 01:43:51 +0530

    Bug fixes, added filters

--- v2/pkg/reporting/issues/issues.go
+++ v2/pkg/reporting/issues/issues.go
@@ -2,6 +2,7 @@ package issues
 
 import (
 	"os"
+	"strings"
 
 	"github.com/pkg/errors"
 	"github.com/projectdiscovery/nuclei/v2/pkg/output"
@@ -14,12 +15,60 @@ import (
 
 // Options is a configuration file for nuclei reporting module
 type Options struct {
+	// AllowList contains a list of allowed events for reporting module
+	AllowList *Filter `yaml:"allow-list"`
+	// DenyList contains a list of denied events for reporting module
+	DenyList *Filter `yaml:"deny-list"`
 	// Github contains configuration options for Github Issue Tracker
 	Github *github.Options `yaml:"github"`
 	// Gitlab contains configuration options for Gitlab Issue Tracker
 	Gitlab *gitlab.Options `yaml:"gitlab"`
 	// Jira contains configuration options for Jira Issue Tracker
 	Jira *jira.Options `yaml:"jira"`
+}
+
+// Filter filters the received event and decides whether to perform
+// reporting for it or not.
+type Filter struct {
+	Severity string `yaml:"severity"`
+	severity []string
+	Tags     string `yaml:"tags"`
+	tags     []string
+}
+
+// Compile compiles the filter creating match structures.
+func (f *Filter) Compile() {
+	parts := strings.Split(f.Severity, ",")
+	for _, part := range parts {
+		f.severity = append(f.severity, strings.TrimSpace(part))
+	}
+	parts = strings.Split(f.Tags, ",")
+	for _, part := range parts {
+		f.tags = append(f.tags, strings.TrimSpace(part))
+	}
+}
+
+// GetMatch returns true if a filter matches result event
+func (f *Filter) GetMatch(event *output.ResultEvent) bool {
+	severity := event.Info["severity"]
+	if len(f.severity) > 0 {
+		if stringSliceContains(f.severity, severity) {
+			return true
+		}
+		return false
+	}
+
+	tags := event.Info["tags"]
+	tagParts := strings.Split(tags, ",")
+	for i, tag := range tagParts {
+		tagParts[i] = strings.TrimSpace(tag)
+	}
+	for _, tag := range f.tags {
+		if stringSliceContains(tagParts, tag) {
+			return true
+		}
+	}
+	return false
 }
 
 // Tracker is an interface implemented by an issue tracker
@@ -31,6 +80,7 @@ type Tracker interface {
 // Client is a client for nuclei issue tracking module
 type Client struct {
 	tracker Tracker
+	options *Options
 	dedupe  *dedupe.Storage
 }
 
@@ -46,6 +96,13 @@ func New(config, db string) (*Client, error) {
 	if err := yaml.NewDecoder(file).Decode(options); err != nil {
 		return nil, err
 	}
+	if options.AllowList != nil {
+		options.AllowList.Compile()
+	}
+	if options.DenyList != nil {
+		options.DenyList.Compile()
+	}
+
 	var tracker Tracker
 	if options.Github != nil {
 		tracker, err = github.New(options.Github)
@@ -66,7 +123,7 @@ func New(config, db string) (*Client, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &Client{tracker: tracker, dedupe: storage}, nil
+	return &Client{tracker: tracker, dedupe: storage, options: options}, nil
 }
 
 // Close closes the issue tracker reporting client
@@ -76,6 +133,13 @@ func (c *Client) Close() {
 
 // CreateIssue creates an issue in the tracker
 func (c *Client) CreateIssue(event *output.ResultEvent) error {
+	if c.options.AllowList != nil && !c.options.AllowList.GetMatch(event) {
+		return nil
+	}
+	if c.options.DenyList != nil && c.options.DenyList.GetMatch(event) {
+		return nil
+	}
+
 	found, err := c.dedupe.Index(event)
 	if err != nil {
 		c.tracker.CreateIssue(event)
@@ -85,4 +149,13 @@ func (c *Client) CreateIssue(event *output.ResultEvent) error {
 		return c.tracker.CreateIssue(event)
 	}
 	return nil
+}
+
+func stringSliceContains(slice []string, item string) bool {
+	for _, i := range slice {
+		if strings.EqualFold(i, item) {
+			return true
+		}
+	}
+	return false
 }
