616ed342edcdee1d7a2c913cc0cfeb1d162980fb
Ice3man543 <nizamulrana@gmail.com>
2021-02-26 14:45:48 +0530

    Misc

--- v2/pkg/protocols/common/expressions/expressions_test.go
+++ v2/pkg/protocols/common/expressions/expressions_test.go
@@ -17,6 +17,7 @@ func TestEvaluate(t *testing.T) {
 		{input: "{{hex_encode(Item)}}", expected: "50494e47", extra: map[string]interface{}{"Item": "PING"}},
 		{input: "{{hex_encode(Item)}}\r\n", expected: "50494e47\r\n", extra: map[string]interface{}{"Item": "PING"}},
 		{input: "{{someTestData}}{{hex_encode('PING')}}", expected: "{{someTestData}}50494e47", extra: map[string]interface{}{}},
+		//	{input: `_IWP_JSON_PREFIX_{{base64('{"iwp_action":"add_site","params":{"username":""}}')}}`, expected: "_IWP_JSON_PREFIX_eyJpd3BfYWN0aW9uIjoiYWRkX3NpdGUiLCJwYXJhbXMiOnsidXNlcm5hbWUiOiIifX0=", extra: map[string]interface{}{}},
 	}
 	for _, item := range items {
 		value, err := Evaluate(item.input, item.extra)
