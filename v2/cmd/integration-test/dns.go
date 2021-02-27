328a4b0e27f5c54527707609b00a25e0847ac79a
Ice3man543 <nizamulrana@gmail.com>
2021-02-27 12:59:26 +0530

    Fixed DNS test + Misc

--- v2/cmd/integration-test/dns.go
+++ v2/cmd/integration-test/dns.go
@@ -1,10 +1,6 @@
 package main
 
 import (
-	"io/ioutil"
-	"strconv"
-
-	"github.com/miekg/dns"
 	"github.com/projectdiscovery/nuclei/v2/internal/testutils"
 )
 
@@ -14,41 +10,11 @@ var dnsTestCases = map[string]testutils.TestCase{
 
 type dnsBasic struct{}
 
-func (h *dnsBasic) ServeDNS(w dns.ResponseWriter, r *dns.Msg) {
-	msg := dns.Msg{}
-	msg.SetReply(r)
-	if r.Question[0].Qtype == dns.TypeCNAME {
-		msg.Authoritative = true
-		domain := msg.Question[0].Name
-		if domain == "test.nuclei." {
-			msg.Answer = append(msg.Answer, &dns.CNAME{
-				Hdr:    dns.RR_Header{Name: domain, Rrtype: dns.TypeCNAME, Class: dns.ClassINET, Ttl: 60},
-				Target: "nuclei.works.",
-			})
-		}
-	}
-	_ = w.WriteMsg(&msg)
-}
-
 // Executes executes a test case and returns an error if occurred
 func (h *dnsBasic) Execute(filePath string) error {
 	var routerErr error
 
-	srv := &dns.Server{Addr: "127.0.0.1:" + strconv.Itoa(54), Net: "udp"}
-	srv.Handler = h
-	go func() {
-		if err := srv.ListenAndServe(); err != nil {
-			routerErr = err
-			return
-		}
-	}()
-
-	err := ioutil.WriteFile("resolvers.txt", []byte("127.0.0.1:54"), 0777)
-	if err != nil {
-		return err
-	}
-
-	results, err := testutils.RunNucleiAndGetResults(filePath, "test.nuclei", debug, "-r", "resolvers.txt")
+	results, err := testutils.RunNucleiAndGetResults(filePath, "one.one.one.one", debug)
 	if err != nil {
 		return err
 	}
