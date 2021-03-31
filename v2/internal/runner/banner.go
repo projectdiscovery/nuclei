931f977c9858b014820d286ec81bbe149ac7b079
sandeep <8293321+ehsandeep@users.noreply.github.com>
2021-04-01 01:46:40 +0530

    version update

--- v2/internal/runner/banner.go
+++ v2/internal/runner/banner.go
@@ -7,11 +7,11 @@ const banner = `
      ____  __  _______/ /__  (_)
     / __ \/ / / / ___/ / _ \/ /
    / / / / /_/ / /__/ /  __/ /
-  /_/ /_/\__,_/\___/_/\___/_/   v2.3.2
+  /_/ /_/\__,_/\___/_/\___/_/   v2.3.3
 `
 
 // Version is the current version of nuclei
-const Version = `2.3.2`
+const Version = `2.3.3`
 
 // showBanner is used to show the banner to the user
 func showBanner() {
