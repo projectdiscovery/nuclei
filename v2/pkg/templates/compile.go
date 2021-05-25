c85377c7359c32977edbe8ee95aab65d5fe1e158
Andrey Sidorov <andrey.sidorov@arrival.com>
2021-05-25 13:13:36 +0300

    exclude tags with tags param

--- v2/pkg/templates/compile.go
+++ v2/pkg/templates/compile.go
@@ -49,14 +49,12 @@ func Parse(filePath string, options protocols.ExecuterOptions) (*Template, error
 	if !ok {
 		templateTags = ""
 	}
-	matchWithTags := false
 	if len(options.Options.Tags) > 0 {
 		if err := matchTemplateWithTags(types.ToString(templateTags), types.ToString(template.Info["severity"]), options.Options.Tags); err != nil {
 			return nil, fmt.Errorf("tags filter not matched %s", templateTags)
 		}
-		matchWithTags = true
 	}
-	if len(options.Options.ExcludeTags) > 0 && !matchWithTags {
+	if len(options.Options.ExcludeTags) > 0 {
 		if err := matchTemplateWithTags(types.ToString(templateTags), types.ToString(template.Info["severity"]), options.Options.ExcludeTags); err == nil {
 			return nil, fmt.Errorf("exclude-tags filter matched %s", templateTags)
 		}
