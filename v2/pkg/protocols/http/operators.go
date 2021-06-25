0da1ac539fa1b6648571d166844f0694ba7177c0
mzack <marco.rivoli.nvh@gmail.com>
2021-06-25 18:37:02 +0200

    Removing check on internal dymanic values

--- v2/pkg/protocols/http/operators.go
+++ v2/pkg/protocols/http/operators.go
@@ -111,9 +111,6 @@ func (r *Request) responseToDSLMap(resp *http.Response, host, matched, rawReq, r
 
 // MakeResultEvent creates a result event from internal wrapped event
 func (r *Request) MakeResultEvent(wrapped *output.InternalWrappedEvent) []*output.ResultEvent {
-	if len(wrapped.OperatorsResult.DynamicValues) > 0 {
-		return nil
-	}
 	results := make([]*output.ResultEvent, 0, len(wrapped.OperatorsResult.Matches)+1)
 
 	// If we have multiple matchers with names, write each of them separately.
