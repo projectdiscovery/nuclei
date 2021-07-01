d55836a3d0e258a7292b51f483c3dc1b678cd4f7
Ice3man543 <nizamulrana@gmail.com>
2021-07-01 18:22:08 +0530

    Added vv flag to display more extra verbose information

--- v2/cmd/nuclei/main.go
+++ v2/cmd/nuclei/main.go
@@ -96,6 +96,7 @@ based on templates offering massive extensibility and ease of use.`)
 	set.IntVar(&options.InteractionsEviction, "interactions-eviction", 60, "Number of seconds to wait before evicting requests from cache")
 	set.IntVar(&options.InteractionsPollDuration, "interactions-poll-duration", 5, "Number of seconds before each interaction poll request")
 	set.IntVar(&options.InteractionsColldownPeriod, "interactions-cooldown-period", 5, "Extra time for interaction polling before exiting")
+	set.BoolVar(&options.VerboseVerbose, "vv", false, "Display Extra Verbose Information")
 	_ = set.Parse()
 
 	if cfgFile != "" {
