04d0f5735c3dd7ebb9b871824a163f6bfaea10cf
Ice3man543 <nizamulrana@gmail.com>
2021-07-30 15:29:12 +0530

    RES-120 Added functional testing implementation

---
+++ v2/cmd/functional-test/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+git checkout master
+cd ../nuclei/
+go build
+cp nuclei ../functional-test/nuclei_main
+git checkout dev
+go build
+cp nuclei ../functional-test/nuclei_dev
+cd ../functional-test
+./functional-test -main ./nuclei_main -dev ./nuclei_dev -testcases testcases.txt
