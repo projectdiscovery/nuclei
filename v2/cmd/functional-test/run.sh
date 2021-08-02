d0251192be0811d5508fe436443ab47a93b2bc7a
sandeep <sandeep@projectdiscovery.io>
2021-08-02 12:12:22 +0530

    minor update

--- v2/cmd/functional-test/run.sh
+++ v2/cmd/functional-test/run.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-currentBranch=$(git branch --show-current)
-
 echo 'Building functional-test binary'
 go build
 
-echo 'Building Nuclei binary from' $currentBranch 'branch'
-go build -o nuclei_$currentBranch ../nuclei
+echo 'Building Nuclei binary from current branch'
+go build -o nuclei_dev ../nuclei
 
 echo 'Installing latest release of nuclei'
 GO111MODULE=on go get -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
 
 echo 'Starting Nuclei functional test'
-./functional-test -main nuclei -dev ./nuclei_$currentBranch -testcases testcases.txt
+./functional-test -main nuclei -dev ./nuclei_dev -testcases testcases.txt
