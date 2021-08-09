7c0fedefb8f112ade4b385d732f7c6c009d1e81f
mzack <marco.rivoli.nvh@gmail.com>
2021-08-09 09:49:07 +0200

    Making map of env vars singleton

--- v2/pkg/protocols/common/generators/env.go
+++ v2/pkg/protocols/common/generators/env.go
@@ -6,13 +6,28 @@ import (
 	"github.com/projectdiscovery/stringsutil"
 )
 
-// EnvVars returns a map with all environment variables into a map
-func EnvVars() map[string]interface{} {
+var envVars map[string]interface{}
+
+func init() {
+	// snapshot env vars at bootstrap
+	envVars = parseEnvVars()
+}
+
+func parseEnvVars() map[string]interface{} {
 	sliceEnvVars := os.Environ()
 	envVars := make(map[string]interface{}, len(sliceEnvVars))
 	for _, envVar := range sliceEnvVars {
 		key, val := stringsutil.Before(envVar, "="), stringsutil.After(envVar, "=")
 		envVars[key] = val
 	}
+	return envVars
+}
+
+// EnvVars returns a map with all environment variables into a map
+func EnvVars() map[string]interface{} {
+	if envVars == nil {
+		envVars = parseEnvVars()
+	}
+
 	return envVars
 }
