6e847e7f6a1a47b37552179e303641e79b738339
forgedhallpass <13679401+forgedhallpass@users.noreply.github.com>
2021-08-18 18:37:43 +0300

    Re-introducing custom template info attribute support within the new struct
    * #259 - dynamic key-value field support for template information
    * #940 - new infos in template
    * #834
    * RES-84

--- v2/pkg/model/model.go
+++ v2/pkg/model/model.go
@@ -11,12 +11,13 @@ import (
 )
 
 type Info struct {
-	Name           string                  `json:"name" yaml:"name"`
-	Authors        StringSlice             `json:"author" yaml:"author"`
-	Tags           StringSlice             `json:"tags" yaml:"tags"`
-	Description    string                  `json:"description" yaml:"description"`
-	Reference      StringSlice             `json:"reference" yaml:"reference"`
-	SeverityHolder severity.SeverityHolder `json:"severity" yaml:"severity"`
+	Name             string                  `json:"name" yaml:"name"`
+	Authors          StringSlice             `json:"author" yaml:"author"`
+	Tags             StringSlice             `json:"tags" yaml:"tags"`
+	Description      string                  `json:"description" yaml:"description"`
+	Reference        StringSlice             `json:"reference" yaml:"reference"`
+	SeverityHolder   severity.SeverityHolder `json:"severity" yaml:"severity"`
+	CustomAttributes map[string]string       `json:"customAttributes,omitempty" yaml:"customAttributes,omitempty"`
 }
 
 // StringSlice represents a single (in-lined) or multiple string value(s).
@@ -42,13 +43,17 @@ func (stringSlice StringSlice) ToSlice() []string {
 	}
 }
 
+func (stringSlice StringSlice) String() string {
+	return strings.Join(stringSlice.ToSlice(), ", ")
+}
+
 func (stringSlice *StringSlice) UnmarshalYAML(unmarshal func(interface{}) error) error {
 	marshalledSlice, err := marshalStringToSlice(unmarshal)
 	if err != nil {
 		return err
 	}
 
-	result := make([]string, len(marshalledSlice))
+	result := make([]string, 0, len(marshalledSlice))
 	//nolint:gosimple,nolintlint //cannot be replaced with result = append(result, slices...) because the values are being normalized
 	for _, value := range marshalledSlice {
 		result = append(result, strings.ToLower(strings.TrimSpace(value))) // TODO do we need to introduce RawStringSlice and/or NormalizedStringSlices?
