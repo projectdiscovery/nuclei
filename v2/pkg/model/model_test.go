571fbf08eb2f0b8e1606d7aa668c32d46402e09b
Ice3man543 <nizamulrana@gmail.com>
2021-08-20 15:11:19 +0530

    Fixed some merge problems + misc docgen

--- v2/pkg/model/model_test.go
+++ v2/pkg/model/model_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/projectdiscovery/nuclei/v2/internal/severity"
 	"github.com/stretchr/testify/assert"
+	"gopkg.in/yaml.v2"
 )
 
 func TestInfoJsonMarshal(t *testing.T) {
@@ -22,5 +23,32 @@ func TestInfoJsonMarshal(t *testing.T) {
 	assert.Nil(t, err)
 
 	expected := `{"name":"Test Template Name","author":["forgedhallpass","ice3man"],"tags":["cve","misc"],"description":"Test description","reference":"reference1","severity":"high"}`
+	assert.Equal(t, expected, string(result))
+}
+
+func TestInfoYamlMarshal(t *testing.T) {
+	info := Info{
+		Name:           "Test Template Name",
+		Authors:        StringSlice{[]string{"forgedhallpass", "ice3man"}},
+		Description:    "Test description",
+		SeverityHolder: severity.SeverityHolder{Severity: severity.High},
+		Tags:           StringSlice{[]string{"cve", "misc"}},
+		Reference:      StringSlice{"reference1"},
+	}
+
+	result, err := yaml.Marshal(&info)
+	assert.Nil(t, err)
+
+	expected := `name: Test Template Name
+author:
+- forgedhallpass
+- ice3man
+tags:
+- cve
+- misc
+description: Test description
+reference: reference1
+severity: high
+`
 	assert.Equal(t, expected, string(result))
 }
