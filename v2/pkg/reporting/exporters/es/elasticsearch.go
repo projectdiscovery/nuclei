9e3fcabead439419695f87a7fe3b982bb2f7cec2
Ice3man543 <nizamulrana@gmail.com>
2021-08-25 13:53:44 +0530

    Removed es exporter cli args + misc changes

--- v2/pkg/reporting/exporters/es/elasticsearch.go
+++ v2/pkg/reporting/exporters/es/elasticsearch.go
@@ -1,35 +1,37 @@
 package es
 
 import (
+	"bytes"
 	"crypto/tls"
-	"errors"
 	"fmt"
 	"io"
 	"net/http"
-	"strings"
-	"sync"
 	"time"
 
-	b64 "encoding/base64"
+	"encoding/base64"
 	"encoding/json"
 
+	"github.com/pkg/errors"
 	"github.com/projectdiscovery/nuclei/v2/pkg/output"
+	"github.com/projectdiscovery/nuclei/v2/pkg/protocols/common/protocolstate"
 )
 
 // Options contains necessary options required for elasticsearch communicaiton
 type Options struct {
-	// Full url for elasticsearch
-	ESIP string
-	// Full url for elasticsearch
-	ESPort int
-	// Enable/Disable SSL
-	ESSSL bool
-	// Enable/DIsable SSL verificaiton
-	ESSSLVerificaiton bool
-	// Elasticsearch username
-	ESUsername string
-	// Elasticsearch password
-	ESPassword string
+	// IP for elasticsearch instance
+	IP string `yaml:"ip"`
+	// Port is the port of elasticsearch instance
+	Port int `yaml:"port"`
+	// SSL enables ssl for elasticsearch connection
+	SSL bool `yaml:"ssl"`
+	// SSLVerification disables SSL verification for elasticsearch
+	SSLVerification bool `yaml:"ssl-verification"`
+	// Username for the elasticsearch instance
+	Username string `yaml:"username"`
+	// Pasword is the password for elasticsearch instance
+	Password string `yaml:"password"`
+	// IndexName is the name of the elasticsearch index
+	IndexName string `yaml:"index-name"`
 }
 
 type data struct {
@@ -39,59 +41,57 @@ type data struct {
 
 // Exporter type for elasticsearch
 type Exporter struct {
-	elasticsearch *http.Client
-	req           *http.Request
-	wg            *sync.Mutex
+	url            string
+	authentication string
+	elasticsearch  *http.Client
 }
 
 // New creates and returns a new exporter for elasticsearch
 func New(option *Options) (*Exporter, error) {
-
 	var ei *Exporter
 
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: option.ESSSLVerificaiton},
+	client := &http.Client{
+		Timeout: 5 * time.Second,
+		Transport: &http.Transport{
+			MaxIdleConns:        10,
+			MaxIdleConnsPerHost: 10,
+			DialContext:         protocolstate.Dialer.Dial,
+			TLSClientConfig:     &tls.Config{InsecureSkipVerify: option.SSLVerification},
+		},
 	}
-	c := &http.Client{
-		Timeout:   5 * time.Second,
-		Transport: tr,
-	}
-
 	// preparing url for elasticsearch
-	url := `http://`
-	if option.ESSSL {
-		url = `https://`
+	scheme := "http://"
+	if option.SSL {
+		scheme = "https://"
 	}
-	url = fmt.Sprintf(url+"%s:%d/nuclei-export/_doc", option.ESIP, option.ESPort)
-
-	// creafting a request
-	req2, err := http.NewRequest(http.MethodPost, url, nil)
-	if err != nil {
-		return ei, nil
-	}
-
 	// if authentication is required
-	if len(option.ESUsername) != 0 && len(option.ESPassword) != 0 {
-		auth := b64.StdEncoding.EncodeToString([]byte(option.ESUsername + ":" + option.ESPassword))
+	var authentication string
+	if len(option.Username) > 0 && len(option.Password) > 0 {
+		auth := base64.StdEncoding.EncodeToString([]byte(option.Username + ":" + option.Password))
 		auth = "Basic " + auth
-		req2.Header.Add("Authorization", auth)
+		authentication = auth
 	}
-	req2.Header.Add("Content-Type", "application/json")
+	url := fmt.Sprintf("%s%s:%d/%s/_doc", scheme, option.IP, option.Port, option.IndexName)
 
 	ei = &Exporter{
-		elasticsearch: c,
-		req:           req2,
-		wg:            &sync.Mutex{},
+		url:            url,
+		authentication: authentication,
+		elasticsearch:  client,
 	}
-
 	return ei, nil
 }
 
 // Export exports a passed result event to disk
 func (i *Exporter) Export(event *output.ResultEvent) error {
-	i.wg.Lock()
-	defer i.wg.Unlock()
-	defer func() { i.req.Body = nil }()
+	// creating a request
+	req, err := http.NewRequest(http.MethodPost, i.url, nil)
+	if err != nil {
+		return errors.Wrap(err, "could not make request")
+	}
+	if len(i.authentication) > 0 {
+		req.Header.Add("Authorization", i.authentication)
+	}
+	req.Header.Add("Content-Type", "application/json")
 
 	d := data{
 		Event:     event,
@@ -101,9 +101,9 @@ func (i *Exporter) Export(event *output.ResultEvent) error {
 	if err != nil {
 		return err
 	}
-	i.req.Body = io.NopCloser(strings.NewReader(string(b)))
+	req.Body = io.NopCloser(bytes.NewReader(b))
 
-	res, err := i.elasticsearch.Do(i.req)
+	res, err := i.elasticsearch.Do(req)
 	b, _ = io.ReadAll(res.Body)
 	if err != nil {
 		return errors.New(err.Error() + "error thrown by elasticsearch " + string(b))
@@ -112,7 +112,6 @@ func (i *Exporter) Export(event *output.ResultEvent) error {
 	if res.StatusCode >= 300 {
 		return errors.New("elasticsearch responded with an error: " + string(b))
 	}
-
 	return nil
 }
 
