7f2e593ce951bccf2c0de452140eba98e9277ce2
sandeep <sandeep@projectdiscovery.io>
2021-09-01 15:53:30 +0530

    final changes

--- v2/pkg/templates/templates.go
+++ v2/pkg/templates/templates.go
@@ -26,7 +26,7 @@ type Template struct {
 	//   example name is `azure-apps-nxdomain-takeover`.
 	// examples:
 	//   - name: ID Example
-	//     value: "\"cve-2021-19520\""
+	//     value: "\"CVE-2021-19520\""
 	ID string `yaml:"id" jsonschema:"title=id of the template,description=The Unique ID for the template,example=cve-2021-19520"`
 	// description: |
 	//   Info contains metadata information about the template.
