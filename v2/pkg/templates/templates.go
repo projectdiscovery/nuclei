5c31b75ac79af453615aaf9cc3279d7496592aca
Ice3man543 <nizamulrana@gmail.com>
2021-09-22 22:41:07 +0530

    Added ssl protocol to nuclei

--- v2/pkg/templates/templates.go
+++ v2/pkg/templates/templates.go
@@ -9,6 +9,7 @@ import (
 	"github.com/projectdiscovery/nuclei/v2/pkg/protocols/headless"
 	"github.com/projectdiscovery/nuclei/v2/pkg/protocols/http"
 	"github.com/projectdiscovery/nuclei/v2/pkg/protocols/network"
+	"github.com/projectdiscovery/nuclei/v2/pkg/protocols/others/ssl"
 	"github.com/projectdiscovery/nuclei/v2/pkg/workflows"
 )
 
@@ -56,6 +57,9 @@ type Template struct {
 	// description: |
 	//   Headless contains the headless request to make in the template.
 	RequestsHeadless []*headless.Request `yaml:"headless,omitempty" json:"headless,omitempty" jsonschema:"title=headless requests to make,description=Headless requests to make for the template"`
+	// description: |
+	//   SSL contains the SSL request to make in the template.
+	RequestsSSL []*ssl.Request `yaml:"ssl,omitempty" json:"ssl,omitempty" jsonschema:"title=ssl requests to make,description=SSL requests to make for the template"`
 
 	// description: |
 	//   Workflows is a yaml based workflow declaration code.
