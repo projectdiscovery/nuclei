1bc400c422d4a183818ef84959772ef56dccd47b
forgedhallpass <13679401+forgedhallpass@users.noreply.github.com>
2021-09-30 20:36:39 +0300

    [feature] Add coloring to debug information #999 [WIP] Return the matched content in case of multiple regex/binary matchers (+test)

--- v2/pkg/operators/matchers/match_test.go
+++ v2/pkg/operators/matchers/match_test.go
@@ -6,15 +6,29 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestANDCondition(t *testing.T) {
+func TestWordANDCondition(t *testing.T) {
 	m := &Matcher{condition: ANDCondition, Words: []string{"a", "b"}}
 
 	isMatched, matched := m.MatchWords("a b", nil)
-	require.True(t, isMatched, "Could not match valid AND condition")
+	require.True(t, isMatched, "Could not match words with valid AND condition")
 	require.Equal(t, m.Words, matched)
 
 	isMatched, matched = m.MatchWords("b", nil)
-	require.False(t, isMatched, "Could match invalid AND condition")
+	require.False(t, isMatched, "Could match words with invalid AND condition")
+	require.Equal(t, []string{}, matched)
+}
+
+func TestRegexANDCondition(t *testing.T) {
+	m := &Matcher{Type: "regex", Condition: "and", Regex: []string{"[a-z]{3}", "\\d{2}"}}
+	err := m.CompileMatchers()
+	require.Nil(t, err)
+
+	isMatched, matched := m.MatchRegex("abc abcd 123")
+	require.True(t, isMatched, "Could not match regex with valid AND condition")
+	require.Equal(t, []string{"abc", "abc", "12"}, matched)
+
+	isMatched, matched = m.MatchRegex("bc 1")
+	require.False(t, isMatched, "Could match regex with invalid AND condition")
 	require.Equal(t, []string{}, matched)
 }
 
@@ -22,15 +36,29 @@ func TestORCondition(t *testing.T) {
 	m := &Matcher{condition: ORCondition, Words: []string{"a", "b"}}
 
 	isMatched, matched := m.MatchWords("a b", nil)
-	require.True(t, isMatched, "Could not match valid OR condition")
+	require.True(t, isMatched, "Could not match valid word OR condition")
 	require.Equal(t, []string{"a"}, matched)
 
 	isMatched, matched = m.MatchWords("b", nil)
-	require.True(t, isMatched, "Could not match valid OR condition")
+	require.True(t, isMatched, "Could not match valid word OR condition")
 	require.Equal(t, []string{"b"}, matched)
 
 	isMatched, matched = m.MatchWords("c", nil)
-	require.False(t, isMatched, "Could match invalid OR condition")
+	require.False(t, isMatched, "Could match invalid word OR condition")
+	require.Equal(t, []string{}, matched)
+}
+
+func TestRegexOrCondition(t *testing.T) {
+	m := &Matcher{Type: "regex", Condition: "or", Regex: []string{"[a-z]{3}", "\\d{2}"}}
+	err := m.CompileMatchers()
+	require.Nil(t, err)
+
+	isMatched, matched := m.MatchRegex("ab 123")
+	require.True(t, isMatched, "Could not match valid regex OR condition")
+	require.Equal(t, []string{"12"}, matched)
+
+	isMatched, matched = m.MatchRegex("bc 1")
+	require.False(t, isMatched, "Could match invalid regex OR condition")
 	require.Equal(t, []string{}, matched)
 }
 
