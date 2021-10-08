9ba3494f93f9cec99308ee5886cd70aa88c61127
forgedhallpass <13679401+forgedhallpass@users.noreply.github.com>
2021-10-08 22:44:40 +0300

    If debug is enabled, show matchers that are not named also #1084 * start the indexing of the matcher names from 1 instead of 0

--- v2/pkg/operators/operators.go
+++ v2/pkg/operators/operators.go
@@ -178,7 +178,7 @@ func getMatcherName(matcher *matchers.Matcher, matcherIndex int) string {
 	if matcher.Name != "" {
 		return matcher.Name
 	} else {
-		return matcher.Type + "-" + strconv.Itoa(matcherIndex)
+		return matcher.Type + "-" + strconv.Itoa(matcherIndex+1) // making the index start from 1 to be more readable
 	}
 }
 
