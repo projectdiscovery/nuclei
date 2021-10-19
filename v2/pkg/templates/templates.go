8ca9da767b969fc93ef2c05a71bb506dfb43e52d
Alexey Zhuchkov <zer0divisi1@gmail.com>
2021-10-20 00:43:33 +0300

    Use simpler regexp for template id validation

--- v2/pkg/templates/templates.go
+++ v2/pkg/templates/templates.go
@@ -27,7 +27,7 @@ type Template struct {
 	// examples:
 	//   - name: ID Example
 	//     value: "\"CVE-2021-19520\""
-	ID string `yaml:"id" jsonschema:"title=id of the template,description=The Unique ID for the template,example=cve-2021-19520,pattern=^(?:[a-zA-Z0-9][a-zA-Z0-9]*-?[a-zA-Z0-9]+?)+$"`
+	ID string `yaml:"id" jsonschema:"title=id of the template,description=The Unique ID for the template,example=cve-2021-19520,pattern=^[A-Za-z0-9-_]+$"`
 	// description: |
 	//   Info contains metadata information about the template.
 	// examples:
