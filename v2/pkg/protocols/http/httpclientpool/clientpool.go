9b7ba02593ca45b50039cf3e5482420fa777f05f
kchason <kchason@users.noreply.github.com>
2021-10-27 12:11:42 -0400

    Switch logic to a shared package

--- v2/pkg/protocols/http/httpclientpool/clientpool.go
+++ v2/pkg/protocols/http/httpclientpool/clientpool.go
@@ -3,10 +3,8 @@ package httpclientpool
 import (
 	"context"
 	"crypto/tls"
-	"crypto/x509"
 	"fmt"
-	"io/ioutil"
-	"log"
+	"github.com/projectdiscovery/nuclei/v2/pkg/protocols/utils"
 	"net"
 	"net/http"
 	"net/http/cookiejar"
@@ -170,26 +168,8 @@ func wrappedGet(options *types.Options, configuration *Configuration) (*retryabl
 		InsecureSkipVerify: true,
 	}
 
-	// Build the TLS config with the client certificate if it has been configured with the appropriate options.
-	// Only one of the options needs to be checked since the validation checks in main.go ensure that all three
-	// files are set if any of the client certification configuration options are.
-	if len(options.ClientCertFile) > 0 {
-		// Load the client certificate using the PEM encoded client certificate and the private key file
-		cert, err := tls.LoadX509KeyPair(options.ClientCertFile, options.ClientKeyFile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		tlsConfig.Certificates = []tls.Certificate{cert}
-
-		// Load the certificate authority PEM certificate into the TLS configuration
-		caCert, err := ioutil.ReadFile(options.ClientCAFile)
-		if err != nil {
-			log.Fatal(err)
-		}
-		caCertPool := x509.NewCertPool()
-		caCertPool.AppendCertsFromPEM(caCert)
-		tlsConfig.RootCAs = caCertPool
-	}
+	// Add the client certificate authentication to the request if it's configured
+	tlsConfig = utils.AddConfiguredClientCertToRequest(tlsConfig, options)
 
 	transport := &http.Transport{
 		DialContext:         Dialer.Dial,
