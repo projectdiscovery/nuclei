8e752d61b6782f908861b3a6aacc660cc1b29dc1
Geeknik Labs <466878+geeknik@users.noreply.github.com>
2021-11-05 10:21:54 -0500

    Update docgen.go
    This regular expression has an unescaped dot before 'com', so it might match more hosts than expected when used here.
    When this is used as a regular expression on a URL, it may match anywhere, and arbitrary hosts may come before or after it. :eyes:

--- v2/cmd/docgen/docgen.go
+++ v2/cmd/docgen/docgen.go
@@ -13,7 +13,7 @@ import (
 	"github.com/projectdiscovery/nuclei/v2/pkg/templates"
 )
 
-var pathRegex = regexp.MustCompile(`github.com/projectdiscovery/nuclei/v2/(?:internal|pkg)/(?:.*/)?([A-Za-z\.]+)`)
+var pathRegex = regexp.MustCompile(`github\.com/projectdiscovery/nuclei/v2/(?:internal|pkg)/(?:.*/)?([A-Za-z\.]+)`)
 
 func main() {
 	// Generate yaml syntax documentation
