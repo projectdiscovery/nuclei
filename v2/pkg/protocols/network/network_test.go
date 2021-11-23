b9e4bb18c6d13d6d32d1aac1961d87255a9a9f2c
Ice3man543 <nizamulrana@gmail.com>
2021-11-23 10:44:31 +0530

    Added additional variables for network + simplified logic

--- v2/pkg/protocols/network/network_test.go
+++ v2/pkg/protocols/network/network_test.go
@@ -17,7 +17,7 @@ func TestNetworkCompileMake(t *testing.T) {
 	templateID := "testing-network"
 	request := &Request{
 		ID:       templateID,
-		Address:  []string{"{{Hostname}}", "{{Hostname}}:8082", "tls://{{Hostname}}:443"},
+		Address:  []string{"tls://{{Hostname}}:443"},
 		ReadSize: 1024,
 		Inputs:   []*Input{{Data: "test-data"}},
 	}
@@ -28,17 +28,8 @@ func TestNetworkCompileMake(t *testing.T) {
 	err := request.Compile(executerOpts)
 	require.Nil(t, err, "could not compile network request")
 
-	require.Equal(t, 3, len(request.addresses), "could not get correct number of input address")
-	t.Run("check-host", func(t *testing.T) {
-		require.Equal(t, "{{Hostname}}", request.addresses[0].ip, "could not get correct host")
-	})
-	t.Run("check-host-with-port", func(t *testing.T) {
-		require.Equal(t, "{{Hostname}}", request.addresses[1].ip, "could not get correct host with port")
-		require.Equal(t, "8082", request.addresses[1].port, "could not get correct port for host")
-	})
+	require.Equal(t, 1, len(request.addresses), "could not get correct number of input address")
 	t.Run("check-tls-with-port", func(t *testing.T) {
-		require.Equal(t, "{{Hostname}}", request.addresses[2].ip, "could not get correct host with port")
-		require.Equal(t, "443", request.addresses[2].port, "could not get correct port for host")
-		require.True(t, request.addresses[2].tls, "could not get correct port for host")
+		require.True(t, request.addresses[0].tls, "could not get correct port for host")
 	})
 }
