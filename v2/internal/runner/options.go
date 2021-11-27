a43b7cccf95fc393f1136ec928189c242672325e
sandeep <sandeep@projectdiscovery.io>
2021-11-28 05:29:01 +0530

    Revert "Fixed stdin input parsing bug (#1286)"
    This reverts commit b59582bde7188932cd9776c5433e43eff4caba4c.

--- v2/internal/runner/options.go
+++ v2/internal/runner/options.go
@@ -71,8 +71,9 @@ func hasStdin() bool {
 		return false
 	}
 
-	isPipedFromChrDev := (stat.Mode() & os.ModeCharDevice) != 0
+	isPipedFromChrDev := (stat.Mode() & os.ModeCharDevice) == 0
 	isPipedFromFIFO := (stat.Mode() & os.ModeNamedPipe) != 0
+
 	return isPipedFromChrDev || isPipedFromFIFO
 }
 
