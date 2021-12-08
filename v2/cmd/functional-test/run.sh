4a51f2d5d79d3747168133ac96866fe495cbe534
LuitelSamikshya <luitelsamikshya1@gmail.com>
2021-12-07 22:30:31 -0600

    changes in run.sh file for windows support

--- v2/cmd/functional-test/run.sh
+++ v2/cmd/functional-test/run.sh
@@ -1,13 +1,34 @@
 #!/bin/bash
 
-echo 'Building functional-test binary'
-go build
+# reading os type from flags
+CURRENT_OS=$1
 
-echo 'Building Nuclei binary from current branch'
-go build -o nuclei_dev ../nuclei
+if [ "${CURRENT_OS}" == "windows" ]
+then
+    echo 'Running on windows platform'
+    echo 'Building functional-test binary'
+    go build
+    
+    echo 'Building Nuclei binary from current branch'
+    go build -o nuclei_dev.exe ../nuclei
+    
+    echo 'Installing latest release of nuclei'
+    go build -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+    
+    echo 'Starting Nuclei functional test'
+    ./functional-test.exe -main ./nuclei.exe -dev ./nuclei_dev.exe -testcases testcases.txt
+else
+    echo 'Building functional-test binary'
+    go build -o functional-test
+    
+    echo 'Building Nuclei binary from current branch'
+    go build -o nuclei_dev ../nuclei
+    
+    echo 'Installing latest release of nuclei'
+    go build -v -o nuclei github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+    
+    echo 'Starting Nuclei functional test'
+    ./functional-test -main ./nuclei -dev ./nuclei_dev -testcases testcases.txt
+fi
 
-echo 'Installing latest release of nuclei'
-GO111MODULE=on go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
 
-echo 'Starting Nuclei functional test'
-./functional-test -main nuclei -dev ./nuclei_dev -testcases testcases.txt
