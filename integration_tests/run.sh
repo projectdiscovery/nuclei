0c52be6f8d1616da32b010d9f015d1f5832caff7
forgedhallpass <13679401+forgedhallpass@users.noreply.github.com>
2021-12-14 18:13:53 +0200

    feat: Re-run failed integration tests in debug mode (#1367)
    * If the tests are executed through GitHub actions and there are failed integration tests, they will be re-executed with verbose output to help figuring out the underlying issues.
    * Added some grouping to make reading the logs easier
    
    Ticket: #1365

--- integration_tests/run.sh
+++ integration_tests/run.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
+echo "::group::Build nuclei"
 rm integration-test nuclei 2>/dev/null
 cd ../v2/cmd/nuclei
 go build
 mv nuclei ../../../integration_tests/nuclei 
+echo "::endgroup::"
+echo "::group::Build nuclei integration-test"
 cd ../integration-test
 go build
 mv integration-test ../../../integration_tests/integration-test 
 cd ../../../integration_tests
+echo "::endgroup::"
 ./integration-test
 if [ $? -eq 0 ]
 then
