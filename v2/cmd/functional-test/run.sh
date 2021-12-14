288048fc9ce29e1853c46b2212664dbd7c577293
forgedhallpass <13679401+forgedhallpass@users.noreply.github.com>
2021-12-14 18:13:53 +0200

    feat: Re-run failed integration tests in debug mode (#1367)
    * If the tests are executed through GitHub actions and there are failed integration tests, they will be re-executed with verbose output to help figuring out the underlying issues.
    * Added some grouping to make reading the logs easier
    
    Ticket: #1365

--- v2/cmd/functional-test/run.sh
+++ v2/cmd/functional-test/run.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-echo 'Building functional-test binary'
+echo "::group::Building functional-test binary"
 go build
+echo "::endgroup::"
 
-echo 'Building Nuclei binary from current branch'
+echo "::group::Building Nuclei binary from current branch"
 go build -o nuclei_dev ../nuclei
+echo "::endgroup::"
 
-echo 'Installing latest release of nuclei'
+echo "::group::Installing latest release of nuclei"
 GO111MODULE=on go install -v github.com/projectdiscovery/nuclei/v2/cmd/nuclei
+echo "::endgroup::"
 
 echo 'Starting Nuclei functional test'
 ./functional-test -main nuclei -dev ./nuclei_dev -testcases testcases.txt
