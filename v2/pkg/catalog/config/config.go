35e91a771d88c06bc3e2e7454398d9cd6ac2c624
sandeep <sandeep@projectdiscovery.io>
2022-02-22 13:14:02 +0530

    version update

--- v2/pkg/catalog/config/config.go
+++ v2/pkg/catalog/config/config.go
@@ -27,7 +27,7 @@ type Config struct {
 const nucleiConfigFilename = ".templates-config.json"
 
 // Version is the current version of nuclei
-const Version = `2.6.1-dev`
+const Version = `2.6.1`
 
 func getConfigDetails() (string, error) {
 	homeDir, err := os.UserHomeDir()
