9efa1f70dc7de891d91f4f3cf1dc527eace72049
Ice3man <nizamulrana@gmail.com>
2022-03-14 13:01:28 +0530

    Split tech names on comma + auto scan docs

--- v2/pkg/protocols/common/automaticscan/doc.go
+++ v2/pkg/protocols/common/automaticscan/doc.go
@@ -2,5 +2,10 @@
 // execution for a nuclei instance.
 //
 // First wappalyzer based technology detection is performed and templates
-// are executed based on the results found.
+// are executed based on the results found. The results of wappalyzer
+// technology detection are lowercased and split on space characters in the name,
+// which are then used as tags for the execution of the templates.
+//
+// The logic is very simple and can be further improved to increase the coverage of
+// this mode of nuclei exection.
 package automaticscan
