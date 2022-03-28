a8c6c201fd1ac216490a700e2c8317fb0742c016
mzack <marco.rivoli.nvh@gmail.com>
2022-03-28 18:23:57 +0200

    adding yamlc support

--- v2/pkg/yaml/preprocess.go
+++ v2/pkg/yaml/preprocess.go
@@ -1,24 +1,32 @@
 package yaml
 
 import (
+	"bufio"
 	"bytes"
+	"log"
 	"os"
 	"regexp"
 	"strings"
+	"text/template"
 
 	"github.com/projectdiscovery/fileutil"
+	"github.com/projectdiscovery/stringsutil"
+	"github.com/rs/xid"
+	"gopkg.in/yaml.v3"
 )
 
-var regexImports = regexp.MustCompile(`(?m)# !include:(.+.yaml)`)
+var (
+	regexImports = regexp.MustCompile(`(?m)# !include:(.+[].yaml|+.yamlc])$`)
+)
 
 // PreProcess all include directives
 func PreProcess(data []byte) ([]byte, error) {
 	// find all matches like !include:path\n
-	matches := regexImports.FindAllSubmatch(data, -1)
+	importMatches := regexImports.FindAllSubmatch(data, -1)
 
 	var replaceItems []string
 
-	for _, match := range matches {
+	for _, match := range importMatches {
 		var (
 			matchString     string
 			includeFileName string
@@ -40,6 +48,23 @@ func PreProcess(data []byte) ([]byte, error) {
 			if err != nil {
 				return nil, err
 			}
+			switch {
+			// if it's yaml, tries to preprocess that too recursively
+			case stringsutil.HasSuffixAny(includeFileName, ".yaml"):
+				if subIncludedFileContent, err := PreProcess(includeFileContent); err == nil {
+					includeFileContent = subIncludedFileContent
+				} else {
+					log.Println(err)
+				}
+			// if it's yamlc, it needs to be compiled
+			case stringsutil.HasSuffixAny(includeFileName, ".yamlc"):
+				if subIncludedFileContent, err := preRender(includeFileContent); err == nil {
+					includeFileContent = subIncludedFileContent
+				} else {
+					log.Println(err)
+				}
+			}
+
 			// pad each line of file content with padBytes
 			includeFileContent = bytes.ReplaceAll(includeFileContent, []byte("\n"), padBytes)
 
@@ -51,4 +76,42 @@ func PreProcess(data []byte) ([]byte, error) {
 	replacer := strings.NewReplacer(replaceItems...)
 
 	return []byte(replacer.Replace(string(data))), nil
+}
+
+func preRender(compileData []byte) ([]byte, error) {
+	// Line comments are yaml code (for now we support only inline lists)
+	// the rest is templating
+	var yamlCode, tplCode bytes.Buffer
+	scanner := bufio.NewScanner(bytes.NewReader(compileData))
+	for scanner.Scan() {
+		codeLine := scanner.Text()
+		switch {
+		case stringsutil.HasPrefixAny(codeLine, "# "):
+			yamlCode.WriteString(strings.TrimPrefix(codeLine, "# ") + "\n")
+		default:
+			tplCode.WriteString(codeLine + "\n")
+		}
+	}
+
+	// unmarshal yaml
+	yamlMap := make(map[string]interface{})
+	if err := yaml.Unmarshal(yamlCode.Bytes(), &yamlMap); err != nil {
+		return compileData, err
+	}
+
+	// randomName
+	id := xid.New()
+	// render template with yaml map
+	tp, err := template.New(id.String()).Parse(tplCode.String())
+	if err != nil {
+		return compileData, err
+	}
+
+	var tplRendered bytes.Buffer
+	err = tp.Execute(&tplRendered, yamlMap)
+	if err != nil {
+		return compileData, err
+	}
+
+	return tplRendered.Bytes(), nil
 }
