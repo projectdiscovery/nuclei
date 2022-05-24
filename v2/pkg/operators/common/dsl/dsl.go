61a1a206328fe5dbe6e4c765e5d7d931d78f74db
Rahmat <Rahmat@192.168.2.13>
2022-05-24 08:50:54 +0700

    Add hmac_sha256 dslExpression

--- v2/pkg/operators/common/dsl/dsl.go
+++ v2/pkg/operators/common/dsl/dsl.go
@@ -497,7 +497,7 @@ func init() {
 		"to_string": makeDslFunction(1, func(args ...interface{}) (interface{}, error) {
 			return types.ToString(args[0]), nil
 		}),
-		"hmacsha256": makeDslFunction(2, func(args ...interface{}) (interface{}, error) {
+		"hmac_sha256": makeDslFunction(2, func(args ...interface{}) (interface{}, error) {
 			h := hmac.New(sha256.New, []byte(args[1].(string)))
 			h.Write([]byte(args[0].(string)))
 			return hex.EncodeToString(h.Sum(nil)), nil
