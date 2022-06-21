7df145612e8f6bf08cca9b2c194dffe789058d80
sandeep <sandeep@projectdiscovery.io>
2022-06-22 00:25:14 +0530

    misc update

--- v2/pkg/protocols/http/validate.go
+++ v2/pkg/protocols/http/validate.go
@@ -4,7 +4,7 @@ import "github.com/pkg/errors"
 
 func (request *Request) validate() error {
 	if request.Race && request.ReqCondition {
-		return errors.New("race and req-condition can't be used together")
+		return errors.New("'race' and 'req-condition' can't be used together")
 	}
 
 	return nil
