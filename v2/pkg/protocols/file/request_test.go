b01b56328df37572a60a91dda1f7b38c029eacbe
Ice3man <nizamulrana@gmail.com>
2022-06-24 23:09:27 +0530

    Added exclude-matchers support for template & matchers (#2218)
    * Added exclude-matchers support for template & matchers
    
    * Fixed panics due to typo
    
    * Added support for only template ID + misc cleanup

--- v2/pkg/protocols/file/request_test.go
+++ v2/pkg/protocols/file/request_test.go
@@ -22,6 +22,10 @@ func TestFileExecuteWithResults(t *testing.T) {
 
 	testutils.Init(options)
 	templateID := "testing-file"
+	executerOpts := testutils.NewMockExecuterOptions(options, &testutils.TemplateInfo{
+		ID:   templateID,
+		Info: model.Info{SeverityHolder: severity.Holder{Severity: severity.Low}, Name: "test"},
+	})
 	request := &Request{
 		ID:          templateID,
 		MaxSize:     "1Gb",
@@ -41,11 +45,8 @@ func TestFileExecuteWithResults(t *testing.T) {
 				Regex: []string{"[0-9]+\\.[0-9]+\\.[0-9]+\\.[0-9]+"},
 			}},
 		},
+		options: executerOpts,
 	}
-	executerOpts := testutils.NewMockExecuterOptions(options, &testutils.TemplateInfo{
-		ID:   templateID,
-		Info: model.Info{SeverityHolder: severity.Holder{Severity: severity.Low}, Name: "test"},
-	})
 	err := request.Compile(executerOpts)
 	require.Nil(t, err, "could not compile file request")
 
