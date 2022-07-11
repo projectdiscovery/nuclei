f1484ed921ff2f58981a8169896917d4b4aae085
Mzack9999 <mzack9999@protonmail.com>
2022-07-11 23:28:36 +0200

    Adding offline http tests (#2276)

--- v2/cmd/integration-test/offline-http.go
+++ v2/cmd/integration-test/offline-http.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/projectdiscovery/nuclei/v2/pkg/testutils"
 )
 
 var offlineHttpTestcases = map[string]testutils.TestCase{
-	"offlinehttp/rfc-req-resp.yaml": &RfcRequestResponse{},
+	"offlinehttp/rfc-req-resp.yaml":          &RfcRequestResponse{},
+	"offlinehttp/offline-allowed-paths.yaml": &RequestResponseWithAllowedPaths{},
+	"offlinehttp/offline-raw.yaml":           &RawRequestResponse{},
 }
 
 type RfcRequestResponse struct{}
@@ -18,4 +22,27 @@ func (h *RfcRequestResponse) Execute(filePath string) error {
 	}
 
 	return expectResultsCount(results, 1)
+}
+
+type RequestResponseWithAllowedPaths struct{}
+
+// Execute executes a test case and returns an error if occurred
+func (h *RequestResponseWithAllowedPaths) Execute(filePath string) error {
+	results, err := testutils.RunNucleiTemplateAndGetResults(filePath, "offlinehttp/data/", debug, "-passive")
+	if err != nil {
+		return err
+	}
+
+	return expectResultsCount(results, 1)
+}
+
+type RawRequestResponse struct{}
+
+// Execute executes a test case and returns an error if occurred
+func (h *RawRequestResponse) Execute(filePath string) error {
+	_, err := testutils.RunNucleiTemplateAndGetResults(filePath, "offlinehttp/data/", debug, "-passive")
+	if err == nil {
+		return fmt.Errorf("incorrect result: no error (actual) vs error expected")
+	}
+	return nil
 }
