6e57b8bee0810f4146c95b5c7bd2e04185f522c7
51pwn <51pwn@51pwn.com>
2022-07-20 12:31:18 +0800

    fix #2301 2022-07-20

--- v2/pkg/protocols/http/request.go
+++ v2/pkg/protocols/http/request.go
@@ -343,6 +343,8 @@ const drainReqSize = int64(8 * 1024)
 
 var errStopExecution = errors.New("stop execution due to unresolved variables")
 
+var someMapMutex = sync.RWMutex{}
+
 // executeRequest executes the actual generated request and returns error if occurred
 func (request *Request) executeRequest(reqURL string, generatedRequest *generatedRequest, previousEvent output.InternalEvent, hasInteractMatchers bool, callback protocols.OutputEventCallback, requestCount int) error {
 	request.setCustomHeaders(generatedRequest)
@@ -578,6 +580,7 @@ func (request *Request) executeRequest(reqURL string, generatedRequest *generate
 		if request.options.Interactsh != nil {
 			request.options.Interactsh.MakePlaceholders(generatedRequest.interactshURLs, outputEvent)
 		}
+		someMapMutex.Lock()
 		for k, v := range previousEvent {
 			finalEvent[k] = v
 		}
@@ -593,7 +596,7 @@ func (request *Request) executeRequest(reqURL string, generatedRequest *generate
 				finalEvent[key] = v
 			}
 		}
-
+		someMapMutex.Unlock()
 		// prune signature internal values if any
 		request.pruneSignatureInternalValues(generatedRequest.meta)
 
