9f32022ad596325441cb9e7442d7d01a44880e0c
Ice3man <nizamulrana@gmail.com>
2022-07-26 22:34:40 +0530

    Replaced strings.Replaced with fasttemplate reducing allocations
    Custom template parsing logic was replaced with fasttemplate package for reducing
    allocations in the replacer.Replace hotpath leading to allocation reduction which
    accounted for 30% of total nuclei allocations.
    
    $ go test -bench=. -benchmem
    goos: darwin
    goarch: arm64
    pkg: github.com/projectdiscovery/nuclei/v2/pkg/protocols/common/replacer
    BenchmarkReplacer-8               837232              1422 ns/op            2112 B/op         31 allocs/op
    BenchmarkReplacerNew-8           3672765               320.3 ns/op            48 B/op          4 allocs/op

--- v2/pkg/protocols/common/replacer/replacer.go
+++ v2/pkg/protocols/common/replacer/replacer.go
@@ -3,32 +3,16 @@ package replacer
 import (
 	"strings"
 
+	"github.com/valyala/fasttemplate"
+
 	"github.com/projectdiscovery/nuclei/v2/pkg/protocols/common/marker"
 	"github.com/projectdiscovery/nuclei/v2/pkg/types"
 )
 
 // Replace replaces placeholders in template with values on the fly.
 func Replace(template string, values map[string]interface{}) string {
-	var replacerItems []string
-
-	builder := &strings.Builder{}
-	for key, val := range values {
-		builder.WriteString(marker.ParenthesisOpen)
-		builder.WriteString(key)
-		builder.WriteString(marker.ParenthesisClose)
-		replacerItems = append(replacerItems, builder.String())
-		builder.Reset()
-		replacerItems = append(replacerItems, types.ToString(val))
-
-		builder.WriteString(marker.General)
-		builder.WriteString(key)
-		builder.WriteString(marker.General)
-		replacerItems = append(replacerItems, builder.String())
-		builder.Reset()
-		replacerItems = append(replacerItems, types.ToString(val))
-	}
-	replacer := strings.NewReplacer(replacerItems...)
-	final := replacer.Replace(template)
+	replaced := fasttemplate.ExecuteStringStd(template, marker.ParenthesisOpen, marker.ParenthesisClose, values)
+	final := fasttemplate.ExecuteStringStd(replaced, marker.General, marker.General, values)
 	return final
 }
 
