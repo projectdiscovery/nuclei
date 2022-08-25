011da1388d34c1f76cce9e03cbb057b53eb7ef61
Sajad <parrasajad@gmail.com>
2022-08-25 17:42:35 +0530

    add option to specify network interface (#2384)
    * add option to specify network interface
    
    * add source-ip flag
    
    * fix typo
    
    * fix err return
    
    * readme update
    
    Co-authored-by: Sandeep Singh <sandeep@projectdiscovery.io>

--- v2/pkg/types/types.go
+++ v2/pkg/types/types.go
@@ -230,6 +230,10 @@ type Options struct {
 	DisableRedirects bool
 	// SNI custom hostname
 	SNI string
+	// Interface to use for network scan
+	Interface string
+	// SourceIP sets custom source IP address for network requests
+	SourceIP string
 	// Health Check
 	HealthCheck bool
 	// Time to wait between each input read operation before closing the stream
