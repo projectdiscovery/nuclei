c1622342ea7026cee0cd742e65e83e5ef25ea075
sandeep <sandeep@projectdiscovery.io>
2022-09-19 00:45:39 +0530

    misc option update

--- v2/cmd/nuclei/main.go
+++ v2/cmd/nuclei/main.go
@@ -271,8 +271,8 @@ on extensive configurability, massive extensibility and ease of use.`)
 
 	flagSet.CreateGroup("cloud", "Cloud",
 		flagSet.BoolVar(&options.Cloud, "cloud", false, "run scan on nuclei cloud"),
-		flagSet.StringVarEnv(&options.CloudURL, "cloud-url", "cu", "http://cloud-dev.nuclei.sh", "NUCLEI_CLOUD_HOST", "url for the nuclei cloud server"),
-		flagSet.StringVarEnv(&options.CloudAPIKey, "cloud-api-key", "cak", "", "NUCLEI_CLOUD_APIKEY", "api-key for the nuclei cloud server"),
+		flagSet.StringVarEnv(&options.CloudURL, "cloud-server", "cs", "http://cloud-dev.nuclei.sh", "NUCLEI_CLOUD_SERVER", "url for the nuclei cloud server"),
+		flagSet.StringVarEnv(&options.CloudAPIKey, "cloud-api-key", "ak", "", "NUCLEI_CLOUD_APIKEY", "api-key for the nuclei cloud server"),
 	)
 
 	_ = flagSet.Parse()
