1261dae73557575a605fc8792688a78571f9d6f5
mzack <marco.rivoli.nvh@gmail.com>
2022-10-02 16:21:22 +0200

    adding comments

--- v2/pkg/protocols/common/contextargs/contextargs.go
+++ v2/pkg/protocols/common/contextargs/contextargs.go
@@ -5,15 +5,20 @@ import (
 	"sync"
 )
 
+// Context implements a shared context struct to share information across multiple templates within a workflow
 type Context struct {
-	Input     string
+	// Input target for the executor
+	Input string
+	// CookieJar shared within workflow's http templates
 	CookieJar *cookiejar.Jar
 
 	// Access to Args must use lock strategies to prevent data races
 	*sync.RWMutex
+	// Args is a workflow shared key-value store
 	Args map[string]interface{}
 }
 
+// Create a new contextargs instance
 func New() Context {
 	return Context{Args: make(map[string]interface{}), RWMutex: &sync.RWMutex{}}
 }
