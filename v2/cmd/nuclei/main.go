e217eeffbb4793fc340eb6d1f3cf0ac75a451569
Ice3man <nizamulrana@gmail.com>
2022-10-07 20:10:00 +0530

    Added response truncation support with flags (#2688)
    * Added response truncation support with flags
    
    * Fixed failing tests for no size

--- v2/cmd/nuclei/main.go
+++ v2/cmd/nuclei/main.go
@@ -194,6 +194,8 @@ on extensive configurability, massive extensibility and ease of use.`)
 		flagSet.StringVarP(&options.Interface, "interface", "i", "", "network interface to use for network scan"),
 		flagSet.StringVarP(&options.SourceIP, "source-ip", "sip", "", "source ip address to use for network scan"),
 		flagSet.StringVar(&options.CustomConfigDir, "config-directory", "", "Override the default config path ($home/.config)"),
+		flagSet.IntVarP(&options.ResponseReadSize, "response-size-read", "rsr", 10*1024*1024, "max response size to read in bytes"),
+		flagSet.IntVarP(&options.ResponseSaveSize, "response-size-save", "rss", 1*1024*1024, "max response size to read in bytes"),
 	)
 
 	flagSet.CreateGroup("interactsh", "interactsh",
--- v2/pkg/types/types.go
+++ v2/pkg/types/types.go
@@ -244,6 +244,10 @@ type Options struct {
 	Interface string
 	// SourceIP sets custom source IP address for network requests
 	SourceIP string
+	// ResponseReadSize is the maximum size of response to read
+	ResponseReadSize int
+	// ResponseSaveSize is the maximum size of response to save
+	ResponseSaveSize int
 	// Health Check
 	HealthCheck bool
 	// Time to wait between each input read operation before closing the stream
