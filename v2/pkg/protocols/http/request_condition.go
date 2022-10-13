26ae7df20dd0c7952a44aae3978a5afb9aacfe40
mzack <marco.rivoli.nvh@gmail.com>
2022-10-13 02:10:37 +0200

    adding missing checks on part

--- v2/pkg/protocols/http/request_condition.go
+++ v2/pkg/protocols/http/request_condition.go
@@ -12,19 +12,30 @@ var (
 // NeedsRequestCondition determines if request condition should be enabled
 func (request *Request) NeedsRequestCondition() bool {
 	for _, matcher := range request.Matchers {
-		for _, dslExpression := range matcher.DSL {
-			if reRequestCondition.MatchString(dslExpression) {
-				return true
-			}
+		if checkRequestConditionExpressions(matcher.DSL...) {
+			return true
+		}
+		if checkRequestConditionExpressions(matcher.Part) {
+			return true
 		}
 	}
 	for _, extractor := range request.Extractors {
-		for _, dslExpression := range extractor.DSL {
-			if reRequestCondition.MatchString(dslExpression) {
-				return true
-			}
+		if checkRequestConditionExpressions(extractor.DSL...) {
+			return true
+		}
+		if checkRequestConditionExpressions(extractor.Part) {
+			return true
 		}
 	}
 
+	return false
+}
+
+func checkRequestConditionExpressions(expressions ...string) bool {
+	for _, expression := range expressions {
+		if reRequestCondition.MatchString(expression) {
+			return true
+		}
+	}
 	return false
 }
