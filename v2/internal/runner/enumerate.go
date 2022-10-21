f4290fd1a60183dd6d6ae03adcba8f5710b0aa0d
Jaideep K <jaideep@one2n.in>
2022-10-21 11:46:15 +0530

    Remove unused constant

--- v2/internal/runner/enumerate.go
+++ v2/internal/runner/enumerate.go
@@ -14,7 +14,6 @@ import (
 	"go.uber.org/atomic"
 )
 
-const timeFormat = "2006-01-02 15:04:05 +0000 UTC"
 const DDMMYYYYhhmmss = "2006-01-02 15:04:05"
 
 // runStandardEnumeration runs standard enumeration
