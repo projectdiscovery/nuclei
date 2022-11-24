b573964f6815f0d93f7dd2a5be55ef3d8709345e
alins.ir <67107893+alins1r@users.noreply.github.com>
2022-11-24 10:08:37 +0330

    Fix a typo
    The description of rss switch was incorrect

--- v2/cmd/nuclei/main.go
+++ v2/cmd/nuclei/main.go
@@ -197,7 +197,7 @@ on extensive configurability, massive extensibility and ease of use.`)
 		flagSet.StringVarP(&options.SourceIP, "source-ip", "sip", "", "source ip address to use for network scan"),
 		flagSet.StringVar(&options.CustomConfigDir, "config-directory", "", "Override the default config path ($home/.config)"),
 		flagSet.IntVarP(&options.ResponseReadSize, "response-size-read", "rsr", 10*1024*1024, "max response size to read in bytes"),
-		flagSet.IntVarP(&options.ResponseSaveSize, "response-size-save", "rss", 1*1024*1024, "max response size to read in bytes"),
+		flagSet.IntVarP(&options.ResponseSaveSize, "response-size-save", "rss", 1*1024*1024, "max response size to save in bytes"),
 	)
 
 	flagSet.CreateGroup("interactsh", "interactsh",
