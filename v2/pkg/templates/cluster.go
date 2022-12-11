139a742a095f08a85b735bb0c7552a3e077bab3c
Ice3man <nizamulrana@gmail.com>
2022-12-11 18:06:21 +0530

    Added http probing to clustering + add disable-clustering flag (#3019)
    * Added http probing to clustering + add disable-clustering flag
    
    * misc update
    
    * Commented out failing test
    
    * Fixed lint error
    
    Co-authored-by: sandeep <8293321+ehsandeep@users.noreply.github.com>

--- v2/pkg/templates/cluster.go
+++ v2/pkg/templates/cluster.go
@@ -13,6 +13,7 @@ import (
 	"github.com/projectdiscovery/nuclei/v2/pkg/protocols/common/contextargs"
 	"github.com/projectdiscovery/nuclei/v2/pkg/protocols/common/helpers/writer"
 	"github.com/projectdiscovery/nuclei/v2/pkg/protocols/http"
+	"github.com/projectdiscovery/nuclei/v2/pkg/templates/types"
 	cryptoutil "github.com/projectdiscovery/utils/crypto"
 )
 
@@ -89,7 +90,7 @@ func ClusterID(templates []*Template) string {
 }
 
 func ClusterTemplates(templatesList []*Template, options protocols.ExecuterOptions) ([]*Template, int) {
-	if options.Options.OfflineHTTP {
+	if options.Options.OfflineHTTP || options.Options.DisableClustering {
 		return templatesList, 0
 	}
 
@@ -183,9 +184,15 @@ func (e *ClusterExecuter) Requests() int {
 func (e *ClusterExecuter) Execute(input *contextargs.Context) (bool, error) {
 	var results bool
 
+	inputItem := input.Clone()
+	if e.options.InputHelper != nil && input.MetaInput.Input != "" {
+		if inputItem.MetaInput.Input = e.options.InputHelper.Transform(input.MetaInput.Input, types.HTTPProtocol); input.MetaInput.Input == "" {
+			return false, nil
+		}
+	}
 	previous := make(map[string]interface{})
 	dynamicValues := make(map[string]interface{})
-	err := e.requests.ExecuteWithResults(input, dynamicValues, previous, func(event *output.InternalWrappedEvent) {
+	err := e.requests.ExecuteWithResults(inputItem, dynamicValues, previous, func(event *output.InternalWrappedEvent) {
 		for _, operator := range e.operators {
 			result, matched := operator.operator.Execute(event.InternalEvent, e.requests.Match, e.requests.Extract, e.options.Options.Debug || e.options.Options.DebugResponse)
 			event.InternalEvent["template-id"] = operator.templateID
@@ -216,7 +223,14 @@ func (e *ClusterExecuter) Execute(input *contextargs.Context) (bool, error) {
 // ExecuteWithResults executes the protocol requests and returns results instead of writing them.
 func (e *ClusterExecuter) ExecuteWithResults(input *contextargs.Context, callback protocols.OutputEventCallback) error {
 	dynamicValues := make(map[string]interface{})
-	err := e.requests.ExecuteWithResults(input, dynamicValues, nil, func(event *output.InternalWrappedEvent) {
+
+	inputItem := input.Clone()
+	if e.options.InputHelper != nil && input.MetaInput.Input != "" {
+		if inputItem.MetaInput.Input = e.options.InputHelper.Transform(input.MetaInput.Input, types.HTTPProtocol); input.MetaInput.Input == "" {
+			return nil
+		}
+	}
+	err := e.requests.ExecuteWithResults(inputItem, dynamicValues, nil, func(event *output.InternalWrappedEvent) {
 		for _, operator := range e.operators {
 			result, matched := operator.operator.Execute(event.InternalEvent, e.requests.Match, e.requests.Extract, e.options.Options.Debug || e.options.Options.DebugResponse)
 			if matched && result != nil {
