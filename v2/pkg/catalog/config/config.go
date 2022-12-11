ba834dca21ab80bd3bb54733ae517eec2f362024
sandeep <8293321+ehsandeep@users.noreply.github.com>
2022-12-11 18:13:35 +0530

    bug fix release

--- v2/pkg/catalog/config/config.go
+++ v2/pkg/catalog/config/config.go
@@ -32,7 +32,7 @@ type Config struct {
 const nucleiConfigFilename = ".templates-config.json"
 
 // Version is the current version of nuclei
-const Version = `2.8.2-dev`
+const Version = `2.8.3`
 
 var customConfigDirectory string
 
