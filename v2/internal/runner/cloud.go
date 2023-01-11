924da4197b6e963e877944e8f12d7aa7ecb28cfb
Siddharth Shashikar <60960197+shashikarsiddharth@users.noreply.github.com>
2023-01-12 01:34:58 +0530

    Use target filepath instead of target dir path to upload targets (#3182)

--- v2/internal/runner/cloud.go
+++ v2/internal/runner/cloud.go
@@ -245,8 +245,8 @@ func (r *Runner) addTarget(location string) error {
 		if d.IsDir() || !strings.EqualFold(filepath.Ext(path), ".txt") {
 			return nil
 		}
-		base := filepath.Base(location)
-		reference, targetErr := r.cloudClient.AddTarget(base, location)
+		base := filepath.Base(path)
+		reference, targetErr := r.cloudClient.AddTarget(base, path)
 		if targetErr != nil {
 			gologger.Error().Msgf("Could not upload %s: %s", location, targetErr)
 		} else if reference != "" {
