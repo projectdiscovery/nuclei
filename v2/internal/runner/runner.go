41eb7f947e970c8d66cd871f946afd623b2e74f9
xm1k3 <xm1k3@protonmail.com>
2023-03-02 21:21:04 +0100

    http.DefaultClient replaced with retryablehttp

--- v2/internal/runner/runner.go
+++ v2/internal/runner/runner.go
@@ -713,7 +713,7 @@ func (r *Runner) displayExecutionInfo(store *loader.Store) {
 }
 
 func (r *Runner) readNewTemplatesWithVersionFile(version string) ([]string, error) {
-	resp, err := http.DefaultClient.Get(fmt.Sprintf("https://raw.githubusercontent.com/projectdiscovery/nuclei-templates/%s/.new-additions", version))
+	resp, err := retryablehttp.DefaultClient().Get(fmt.Sprintf("https://raw.githubusercontent.com/projectdiscovery/nuclei-templates/%s/.new-additions", version))
 	if err != nil {
 		return nil, err
 	}
