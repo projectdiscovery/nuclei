5395cc990e9f1c576563dc2c167106b89670b471
xm1k3 <xm1k3@protonmail.com>
2023-03-02 09:37:42 +0100

    added InitNucleiVersion

--- v2/internal/runner/update.go
+++ v2/internal/runner/update.go
@@ -90,6 +90,7 @@ func (r *Runner) updateTemplates() error { // TODO this method does more than ju
 		return nil
 	}
 
+	client.InitNucleiVersion(config.Version)
 	r.fetchLatestVersionsFromGithub(configDir) // also fetch the latest versions
 
 	ctx := context.Background()
