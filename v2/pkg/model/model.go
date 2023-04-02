05ca32f8b61e8624adb81f47a942d9126598f29b
sandeep <8293321+ehsandeep@users.noreply.github.com>
2023-04-02 13:10:57 +0530

    Added EPSSScore to Classification

--- v2/pkg/model/model.go
+++ v2/pkg/model/model.go
@@ -96,6 +96,11 @@ type Classification struct {
 	//   - value: "\"9.8\""
 	CVSSScore float64 `json:"cvss-score,omitempty" yaml:"cvss-score,omitempty" jsonschema:"title=cvss score for the template,description=CVSS Score for the template,example=9.8"`
 	// description: |
+	//   EPSS Score for the template.
+	// examples:
+	//   - value: "\"0.42509\""
+	EPSSScore float64 `json:"epss-score,omitempty" yaml:"epss-score,omitempty" jsonschema:"title=epss score for the template,description=EPSS Score for the template,example=0.42509"`
+	// description: |
 	//   CPE for the template.
 	// examples:
 	//   - value: "\"cpe:/a:vendor:product:version\""
