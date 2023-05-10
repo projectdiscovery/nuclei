cee1a2e9831a0ec04873616e71c58a3fb15c4cc5
Shubham Rasal <shubham@projectdiscovery.io>
2023-05-11 03:26:29 +0530

    Add payload in dns protocol (#3632)
    * add execute function in dns
    
    * Add payload in dns protocol
    
    * Add integration test to cover dns payload
    
    - also check command line overriding a payload variable
    
    * Update matchedAt and remove trailing dot
    
    * Consider payload data for request count
    
    - Update verbose output to print question
    - Update dns requests Requests function to consider payload data
    
    * update gitignore
    
    * bump nuclei version to v2.9.4-dev
    
    ---------
    
    Co-authored-by: Tarun Koyalwar <tarun@projectdiscovery.io>

--- v2/pkg/protocols/dns/dns_test.go
+++ v2/pkg/protocols/dns/dns_test.go
@@ -35,3 +35,55 @@ func TestDNSCompileMake(t *testing.T) {
 	require.Nil(t, err, "could not make dns request")
 	require.Equal(t, "one.one.one.one.", req.Question[0].Name, "could not get correct dns question")
 }
+
+func TestDNSRequests(t *testing.T) {
+	options := testutils.DefaultOptions
+
+	recursion := false
+	testutils.Init(options)
+	const templateID = "testing-dns"
+
+	t.Run("dns-regular", func(t *testing.T) {
+
+		request := &Request{
+			RequestType: DNSRequestTypeHolder{DNSRequestType: A},
+			Class:       "INET",
+			Retries:     5,
+			ID:          templateID,
+			Recursion:   &recursion,
+			Name:        "{{FQDN}}",
+		}
+		executerOpts := testutils.NewMockExecuterOptions(options, &testutils.TemplateInfo{
+			ID:   templateID,
+			Info: model.Info{SeverityHolder: severity.Holder{Severity: severity.Low}, Name: "test"},
+		})
+		err := request.Compile(executerOpts)
+		require.Nil(t, err, "could not compile dns request")
+
+		reqCount := request.Requests()
+		require.Equal(t, 1, reqCount, "could not get correct dns request count")
+	})
+
+	// test payload requests count is correct
+	t.Run("dns-payload", func(t *testing.T) {
+
+		request := &Request{
+			RequestType: DNSRequestTypeHolder{DNSRequestType: A},
+			Class:       "INET",
+			Retries:     5,
+			ID:          templateID,
+			Recursion:   &recursion,
+			Name:        "{{subdomain}}.{{FQDN}}",
+			Payloads:    map[string]interface{}{"subdomain": []string{"a", "b", "c"}},
+		}
+		executerOpts := testutils.NewMockExecuterOptions(options, &testutils.TemplateInfo{
+			ID:   templateID,
+			Info: model.Info{SeverityHolder: severity.Holder{Severity: severity.Low}, Name: "test"},
+		})
+		err := request.Compile(executerOpts)
+		require.Nil(t, err, "could not compile dns request")
+
+		reqCount := request.Requests()
+		require.Equal(t, 3, reqCount, "could not get correct dns request count")
+	})
+}
