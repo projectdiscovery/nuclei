a5b04dbb534dc636d9472f3065a9892abfcc5837
Tarun Koyalwar <tarun@projectdiscovery.io>
2023-06-01 17:17:47 +0530

    fix build failure

--- v2/pkg/templates/multiproto_test.go
+++ v2/pkg/templates/multiproto_test.go
@@ -1,14 +1,1 @@
 package templates
-
-// TODO: add more tests
-// unmarshall json and yaml templates for multi protocol requests
-
-// func TestUnmarshalMulti(t *testing.T) {
-// 	tpath := "/Users/tarun/test-templates/multi.yaml"
-// 	bin, err := os.ReadFile(tpath)
-// 	require.Nil(t, err, "failed to load example template")
-// 	var yamlTemplate Template
-// 	err = yaml.Unmarshal(bin, &yamlTemplate)
-// 	require.Nil(t, err, "failed to unmarshal yaml template")
-// 	t.Log(yamlTemplate.MultiProtoRequest.Queue)
-// }
