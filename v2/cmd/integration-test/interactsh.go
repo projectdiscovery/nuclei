a94e3853707cebbc895d70f3cb244521c1392478
Doğan Can Bakır <dogancanbakir@protonmail.com>
2023-07-19 13:08:48 +0000

    disable interactsh tests on darwin

--- v2/cmd/integration-test/interactsh.go
+++ v2/cmd/integration-test/interactsh.go
@@ -2,7 +2,7 @@ package main
 
 // All Interactsh related testcases
 var interactshTestCases = []TestCaseInfo{
-	{DisableOn: []string{"windows"}, Path: "http/interactsh.yaml", TestCase: &httpInteractshRequest{}},
-	{DisableOn: []string{"windows"}, Path: "http/interactsh-stop-at-first-match.yaml", TestCase: &httpInteractshStopAtFirstMatchRequest{}},
-	{DisableOn: []string{"windows"}, Path: "http/default-matcher-condition.yaml", TestCase: &httpDefaultMatcherCondition{}},
+	{DisableOn: []string{"windows", "darwin"}, Path: "http/interactsh.yaml", TestCase: &httpInteractshRequest{}},
+	{DisableOn: []string{"windows", "darwin"}, Path: "http/interactsh-stop-at-first-match.yaml", TestCase: &httpInteractshStopAtFirstMatchRequest{}},
+	{DisableOn: []string{"windows", "darwin"}, Path: "http/default-matcher-condition.yaml", TestCase: &httpDefaultMatcherCondition{}},
 }
