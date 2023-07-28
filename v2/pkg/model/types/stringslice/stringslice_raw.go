89cac337eec41f0e5ecd1246e643c8e7000e8d10
Keith Chason <kchason@users.noreply.github.com>
2023-07-28 10:18:15 -0400

    Omit Empty Fields (#3977)
    * Don't show Lines/matched-line on null
    
    * Remove unused "info.references" property
    
    * Revert "Remove unused "info.references" property"
    
    This reverts commit 6466644bcac6952ece8d2bc880ea9157f2e10c16.
    
    * Switch to pointer so omitempty works properly
    
    * keeping matcher status output in jsonl output always
    
    * rename function to NewRawStringSlice
    
    ---------
    
    Co-authored-by: sandeep <8293321+ehsandeep@users.noreply.github.com>
    Co-authored-by: Tarun Koyalwar <tarun@projectdiscovery.io>

--- v2/pkg/model/types/stringslice/stringslice_raw.go
+++ v2/pkg/model/types/stringslice/stringslice_raw.go
@@ -4,10 +4,10 @@ type RawStringSlice struct {
 	StringSlice
 }
 
-func NewRaw(value interface{}) RawStringSlice {
-	return RawStringSlice{StringSlice: StringSlice{Value: value}}
+func NewRawStringSlice(value interface{}) *RawStringSlice {
+	return &RawStringSlice{StringSlice: StringSlice{Value: value}}
 }
 
-func (rawStringSlice RawStringSlice) Normalize(value string) string {
+func (rawStringSlice *RawStringSlice) Normalize(value string) string {
 	return value
 }
