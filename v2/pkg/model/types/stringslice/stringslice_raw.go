4631f873d7143b6f2c105368eb3bbbef0f643848
Tarun Koyalwar <tarun@projectdiscovery.io>
2023-07-28 19:32:27 +0530

    rename function to NewRawStringSlice

--- v2/pkg/model/types/stringslice/stringslice_raw.go
+++ v2/pkg/model/types/stringslice/stringslice_raw.go
@@ -4,15 +4,10 @@ type RawStringSlice struct {
 	StringSlice
 }
 
-func NewRaw(value interface{}) RawStringSlice {
-	return RawStringSlice{StringSlice: StringSlice{Value: value}}
+func NewRawStringSlice(value interface{}) *RawStringSlice {
+	return &RawStringSlice{StringSlice: StringSlice{Value: value}}
 }
 
-func NewPointer(value interface{}) *RawStringSlice {
-	s := NewRaw(value)
-	return &s
-}
-
-func (rawStringSlice RawStringSlice) Normalize(value string) string {
+func (rawStringSlice *RawStringSlice) Normalize(value string) string {
 	return value
 }
