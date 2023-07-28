5c5f9747eea7d4210875bf8604ee23f9c6f7572b
Tarun Koyalwar <45962551+tarunKoyalwar@users.noreply.github.com>
2023-07-28 19:28:20 +0530

    headless: automerge and other improvements (#3958)
    * headless: automerge and other improvements
    
    * fix typo in function signature

--- v2/pkg/protocols/headless/engine/instance.go
+++ v2/pkg/protocols/headless/engine/instance.go
@@ -17,6 +17,7 @@ type Instance struct {
 
 	// redundant due to dependency cycle
 	interactsh *interactsh.Client
+	requestLog map[string]string // contains actual request that was sent
 }
 
 // NewInstance creates a new instance for the current browser.
@@ -35,7 +36,14 @@ func (b *Browser) NewInstance() (*Instance, error) {
 	// We use a custom sleeper that sleeps from 100ms to 500 ms waiting
 	// for an interaction. Used throughout rod for clicking, etc.
 	browser = browser.Sleeper(func() utils.Sleeper { return maxBackoffSleeper(10) })
-	return &Instance{browser: b, engine: browser}, nil
+	return &Instance{browser: b, engine: browser, requestLog: map[string]string{}}, nil
+}
+
+// returns a map of [template-defined-urls] -> [actual-request-sent]
+// Note: this does not include CORS or other requests while rendering that were not explicitly
+// specified in template
+func (i *Instance) GetRequestLog() map[string]string {
+	return i.requestLog
 }
 
 // Close closes all the tabs and pages for a browser instance
