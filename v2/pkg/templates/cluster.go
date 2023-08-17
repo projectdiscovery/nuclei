1ee108ed13f21b57c529d27126c482bbda914af3
seeyarh <collinsrhuff@gmail.com>
2023-08-17 13:35:15 -0400

    include request and response when matcher status is false (#3986)
    * include request and response when matcher status is false
    
    * use failed result event to write failure
    
    ---------
    
    Co-authored-by: Collins Huff <collins.huff@Collinss-MacBook-Pro-2.local>
    Co-authored-by: Tarun Koyalwar <tarun@projectdiscovery.io>

--- v2/pkg/templates/cluster.go
+++ v2/pkg/templates/cluster.go
@@ -250,7 +250,7 @@ func (e *ClusterExecuter) Execute(input *contextargs.Context) (bool, error) {
 			event.InternalEvent["template-info"] = operator.templateInfo
 
 			if result == nil && !matched {
-				if err := e.options.Output.WriteFailure(event.InternalEvent); err != nil {
+				if err := e.options.Output.WriteFailure(event); err != nil {
 					gologger.Warning().Msgf("Could not write failure event to output: %s\n", err)
 				}
 				continue
