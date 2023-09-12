d58343c27709bc3128a63d0b03232e1bee1ccbe6
Tarun Koyalwar <tarun@projectdiscovery.io>
2023-09-12 21:06:44 +0530

    final docs update

--- v2/pkg/js/generated/js/global.js
+++ v2/pkg/js/generated/js/global.js
@@ -3,6 +3,8 @@
  * Rand(n int) []byte
  * @function
  * @param {number} n - The length of the byte slice.
+ * @example
+ * let myint = Rand(10); // returns a random byte slice of length 10
  */
 function Rand(n) {
     // implemented in go
@@ -12,6 +14,8 @@ function Rand(n) {
  * RandInt returns a random int
  * RandInt() int
  * @function
+ * @example
+ * let myint = RandInt(); // returns a random int
  */
 function RandInt() {
     // implemented in go
@@ -23,6 +27,9 @@ function RandInt() {
  * log(msg map[string]interface{})
  * @function
  * @param {string|Object} msg - The message to print.
+ * @example
+ * log("Hello World!");
+ * log({"Hello": "World!"});
  */
 function log(msg) {
     // implemented in go
@@ -34,6 +41,8 @@ function log(msg) {
  * @function
  * @param {string} port - The port to check.
  * @param {string} defaultPort - The default port to return if the given port is colliding.
+ * @example
+ * let port = getNetworkPort(Port, "2843"); // 2843 is default port (even if 80,443 etc is given in Port from input)
  */
 function getNetworkPort(port, defaultPort) {
     // implemented in go
@@ -46,6 +55,9 @@ function getNetworkPort(port, defaultPort) {
  * @param {string} host - The host to check.
  * @param {string} port - The port to check.
  * @param {number} [timeout=5] - The timeout in seconds.
+ * @example
+ * let open = isPortOpen("localhost", "80"); // returns true if port 80 is open on localhost
+ * let open = isPortOpen("localhost", "80", 10); // returns true if port 80 is open on localhost within 10 seconds
  */
 function isPortOpen(host, port, timeout = 5) {
     // implemented in go
@@ -56,6 +68,8 @@ function isPortOpen(host, port, timeout = 5) {
  * ToBytes(...interface{}) []byte
  * @function
  * @param {...any} args - The input to convert.
+ * @example
+ * let mybytes = ToBytes("Hello World!"); // returns byte slice of "Hello World!"
  */
 function ToBytes(...args) {
     // implemented in go
@@ -66,6 +80,8 @@ function ToBytes(...args) {
  * ToString(...interface{}) string
  * @function
  * @param {...any} args - The input to convert.
+ * @example
+ * let mystring = ToString([0x48, 0x65, 0x6c, 0x6c, 0x6f]); // returns "Hello"
  */
 function ToString(...args) {
     // implemented in go
