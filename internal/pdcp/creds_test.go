938191362e790a9217e6745a6ed49aef1e94714d
Tarun Koyalwar <tarun@projectdiscovery.io>
2023-11-06 21:23:54 +0530

    fix creds test

--- internal/pdcp/creds_test.go
+++ internal/pdcp/creds_test.go
@@ -1,14 +1,28 @@
 package pdcp
 
 import (
+	"os"
+	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
+var exampleCred = `
+- username: test
+  api-key: testpassword
+  server: https://scanme.sh
+`
+
 func TestLoadCreds(t *testing.T) {
 	// temporarily change PDCP file location for testing
-	PDCPCredFile = "creds.yaml"
+	f, err := os.CreateTemp("", "creds-test-*")
+	require.Nil(t, err)
+	_, _ = f.WriteString(strings.TrimSpace(exampleCred))
+	defer os.Remove(f.Name())
+	PDCPCredFile = f.Name()
+	PDCPDir = filepath.Dir(f.Name())
 	h := &PDCPCredHandler{}
 	value, err := h.GetCreds()
 	require.Nil(t, err)
