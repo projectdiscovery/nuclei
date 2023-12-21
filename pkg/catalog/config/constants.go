a7ba7cbeee621de5dadbe64e071464f15e8f79fa
sandeep <8293321+ehsandeep@users.noreply.github.com>
2023-12-21 18:18:58 +0530

    version update

--- pkg/catalog/config/constants.go
+++ pkg/catalog/config/constants.go
@@ -17,7 +17,7 @@ const (
 	CLIConfigFileName               = "config.yaml"
 	ReportingConfigFilename         = "reporting-config.yaml"
 	// Version is the current version of nuclei
-	Version = `v3.1.2`
+	Version = `v3.1.4-dev`
 	// Directory Names of custom templates
 	CustomS3TemplatesDirName     = "s3"
 	CustomGitHubTemplatesDirName = "github"
