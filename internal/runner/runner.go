05172a554571eb9a57189eae66caf20da2e58bcc
Tarun Koyalwar <tarun@projectdiscovery.io>
2024-01-12 02:44:20 +0530

    wait for exporters when closing

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -328,8 +328,12 @@ func (r *Runner) runStandardEnumeration(executerOpts protocols.ExecutorOptions, 
 
 // Close releases all the resources and cleans up
 func (r *Runner) Close() {
+	gologger.Info().Msgf("Attempting graceful shutdown...")
 	if r.output != nil {
 		r.output.Close()
+	}
+	if r.issuesClient != nil {
+		r.issuesClient.Close()
 	}
 	if r.projectFile != nil {
 		r.projectFile.Close()
@@ -341,6 +345,10 @@ func (r *Runner) Close() {
 	}
 	if r.rateLimiter != nil {
 		r.rateLimiter.Stop()
+	}
+	r.progress.Stop()
+	if r.browser != nil {
+		r.browser.Close()
 	}
 }
 
@@ -494,22 +502,14 @@ func (r *Runner) RunEnumeration() error {
 			results.CompareAndSwap(false, true)
 		}
 	}
-	r.progress.Stop()
-
 	if executorOpts.InputHelper != nil {
 		_ = executorOpts.InputHelper.Close()
-	}
-	if r.issuesClient != nil {
-		r.issuesClient.Close()
 	}
 
 	// todo: error propagation without canonical straight error check is required by cloud?
 	// use safe dereferencing to avoid potential panics in case of previous unchecked errors
 	if v := ptrutil.Safe(results); !v.Load() {
 		gologger.Info().Msgf("No results found. Better luck next time!")
-	}
-	if r.browser != nil {
-		r.browser.Close()
 	}
 	// check if a passive scan was requested but no target was provided
 	if r.options.OfflineHTTP && len(r.options.Targets) == 0 && r.options.TargetsFilePath == "" {
