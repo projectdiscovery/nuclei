d74af440359d5246f6a32dbe16e778662ca6ba86
Tarun Koyalwar <tarun@projectdiscovery.io>
2024-02-29 03:09:18 +0530

    add multipart form + body form unit tests

--- pkg/fuzz/component/body_test.go
+++ pkg/fuzz/component/body_test.go
@@ -1,7 +1,10 @@
 package component
 
 import (
+	"bytes"
 	"io"
+	"mime/multipart"
+	"net/url"
 	"strings"
 	"testing"
 
@@ -74,4 +77,99 @@ func TestBodyXMLComponent(t *testing.T) {
 		t.Fatal(err)
 	}
 	require.Equal(t, "<?xml version=\"1.0\" encoding=\"UTF-8\"?><stockCheck><productId>2'6842</productId><storeId>1</storeId></stockCheck>", string(newBody), "unexpected body")
+}
+
+func TestBodyFormComponent(t *testing.T) {
+	formData := url.Values{}
+	formData.Set("key1", "value1")
+	formData.Set("key2", "value2")
+
+	req, err := retryablehttp.NewRequest("POST", "https://example.com", strings.NewReader(formData.Encode()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	body := New(RequestBodyComponent)
+	_, err = body.Parse(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var keys []string
+	var values []string
+	_ = body.Iterate(func(key string, value interface{}) error {
+		keys = append(keys, key)
+		values = append(values, value.(string))
+		return nil
+	})
+
+	require.ElementsMatch(t, []string{"key1", "key2"}, keys, "unexpected keys")
+	require.ElementsMatch(t, []string{"value1", "value2"}, values, "unexpected values")
+
+	_ = body.SetValue("key1", "updatedValue1")
+
+	rebuilt, err := body.Rebuild()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	newBody, err := io.ReadAll(rebuilt.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	require.Equal(t, "key1=updatedValue1&key2=value2", string(newBody), "unexpected body")
+}
+
+func TestMultiPartFormComponent(t *testing.T) {
+	formData := &bytes.Buffer{}
+	writer := multipart.NewWriter(formData)
+
+	// Hypothetical form fields
+	_ = writer.WriteField("username", "testuser")
+	_ = writer.WriteField("password", "testpass")
+
+	contentType := writer.FormDataContentType()
+	_ = writer.Close()
+
+	req, err := retryablehttp.NewRequest("POST", "https://example.com", formData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", contentType)
+
+	body := New(RequestBodyComponent)
+	_, err = body.Parse(req)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var keys []string
+	var values []string
+	_ = body.Iterate(func(key string, value interface{}) error {
+		keys = append(keys, key)
+		values = append(values, value.(string))
+		return nil
+	})
+
+	require.ElementsMatch(t, []string{"username", "password"}, keys, "unexpected keys")
+	require.ElementsMatch(t, []string{"testuser", "testpass"}, values, "unexpected values")
+
+	// Update a value in the form
+	_ = body.SetValue("password", "updatedTestPass")
+
+	rebuilt, err := body.Rebuild()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	newBody, err := io.ReadAll(rebuilt.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Check if the body contains the updated multipart form data
+	require.Contains(t, string(newBody), "updatedTestPass", "unexpected body content")
+	require.Contains(t, string(newBody), "username", "unexpected body content")
+	require.Contains(t, string(newBody), "testuser", "unexpected body content")
 }
