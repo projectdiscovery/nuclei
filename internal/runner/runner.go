673ae84b973e55a18b508d7cfa06a252bf35d1b0
Doğan Can Bakır <dogancanbakir@protonmail.com>
2024-03-01 01:18:51 +0300

    introduce `disable-unsigned-templates` flag

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -455,6 +455,17 @@ func (r *Runner) RunEnumeration() error {
 		return nil // exit
 	}
 	store.Load()
+
+	if r.options.DisableUnsignedTemplates {
+		unverifiedTemplates := 0
+		for _, template := range store.Templates() {
+			if !template.Verified {
+				unverifiedTemplates++
+			}
+		}
+		gologger.Fatal().Msgf("Found %d unsigned templates: Program exiting.", unverifiedTemplates)
+	}
+
 	// TODO: remove below functions after v3 or update warning messages
 	disk.PrintDeprecatedPathsMsgIfApplicable(r.options.Silent)
 	templates.PrintDeprecatedProtocolNameMsgIfApplicable(r.options.Silent, r.options.Verbose)
