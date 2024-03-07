9aacbe8b82dc4fcddf1c85370f9ae3e70a644ad4
mzack <marco.rivoli.nvh@gmail.com>
2024-03-07 17:28:24 +0100

    small change in calculation

--- pkg/protocols/common/protocolstate/memguardian.go
+++ pkg/protocols/common/protocolstate/memguardian.go
@@ -1,6 +1,13 @@
 package protocolstate
 
-import "github.com/projectdiscovery/utils/memguardian"
+import (
+	"github.com/projectdiscovery/utils/env"
+	"github.com/projectdiscovery/utils/memguardian"
+)
+
+var (
+	MaxThreadsOnLowMemory = env.GetEnvOrDefault("MEMGUARDIAN_THREADS", 0)
+)
 
 func IsLowOnMemory() bool {
 	if memguardian.DefaultMemGuardian != nil && memguardian.DefaultMemGuardian.Warning.Load() {
@@ -8,4 +15,17 @@ func IsLowOnMemory() bool {
 	}
 
 	return false
+}
+
+func GuardThreads(current int) int {
+	if MaxThreadsOnLowMemory > 0 {
+		return MaxThreadsOnLowMemory
+	}
+
+	fraction := int(current / 5)
+	if fraction > 0 {
+		return fraction
+	}
+
+	return 1
 }
