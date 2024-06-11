ac0107c24202a009db8e3b5a7c37db4e786072f3
Mzack9999 <mzack9999@protonmail.com>
2024-06-11 14:58:58 +0200

    revert

--- pkg/installer/versioncheck.go
+++ pkg/installer/versioncheck.go
@@ -68,7 +68,6 @@ func getpdtmParams(isSDK bool) string {
 
 // UpdateIgnoreFile updates default ignore file by downloading latest ignore file
 func UpdateIgnoreFile() error {
-	return nil
 	resp, err := retryableHttpClient.Get(pdtmNucleiIgnoreFileEndpoint + "?" + getpdtmParams(false))
 	if err != nil {
 		return err
@@ -84,11 +83,11 @@ func UpdateIgnoreFile() error {
 }
 
 func doVersionCheck(isSDK bool) error {
-	return nil
 	// we use global retryablehttp client so its not immeditely gc'd if any references are held
 	// and according our config we have idle connections which are shown as leaked by goleak in tests
 	// i.e we close all idle connections after our use and it doesn't affect any other part of the code
 	defer retryableHttpClient.HTTPClient.CloseIdleConnections()
+
 	resp, err := retryableHttpClient.Get(pdtmNucleiVersionEndpoint + "?" + getpdtmParams(isSDK))
 	if err != nil {
 		return err
