5c44b44798c254be6c71d374afd8c86f5a71da86
Ice3man <nizamulrana@gmail.com>
2024-06-16 19:14:43 +0530

    fix: fixed individual per tracker reporting filters (#5297)
    * fix: fixed individual per tracker reporting filters
    
    * added test case

--- pkg/reporting/reporting.go
+++ pkg/reporting/reporting.go
@@ -273,7 +273,7 @@ func (c *ReportingClient) CreateIssue(event *output.ResultEvent) error {
 
 		for _, tracker := range c.trackers {
 			// process tracker specific allow/deny list
-			if tracker.ShouldFilter(event) {
+			if !tracker.ShouldFilter(event) {
 				continue
 			}
 
