a40b0275aa3cec07dfe493fe738e61aec53bcc49
Kristinn Vikar Jónsson <93918469+KristinnVikar@users.noreply.github.com>
2024-06-27 07:44:43 +0000

    Clustering performance improvements (#5319)
    * Clustering performance improvements
    
    * IsClusterable filters out beforehand, update test to mirror that
    
    * inverse IsClusterable
    This makes much more sense
    
    * HashMap based clustering
    
    * furthur improvements to clustering
    
    ---------
    
    Co-authored-by: Tarun Koyalwar <tarun@projectdiscovery.io>

--- pkg/protocols/http/cluster_test.go
+++ pkg/protocols/http/cluster_test.go
@@ -8,8 +8,11 @@ import (
 
 func TestCanCluster(t *testing.T) {
 	req := &Request{Unsafe: true}
-	require.False(t, req.CanCluster(&Request{}), "could cluster unsafe request")
+	require.False(t, req.IsClusterable(), "could cluster unsafe request")
 
 	req = &Request{Path: []string{"{{BaseURL}}"}, Method: HTTPMethodTypeHolder{MethodType: HTTPGet}}
-	require.True(t, req.CanCluster(&Request{Path: []string{"{{BaseURL}}"}, Method: HTTPMethodTypeHolder{MethodType: HTTPGet}}), "could not cluster GET request")
+	newReq := &Request{Path: []string{"{{BaseURL}}"}, Method: HTTPMethodTypeHolder{MethodType: HTTPGet}}
+	require.True(t, req.IsClusterable(), "could not cluster GET request")
+	require.True(t, req.IsClusterable(), "could not cluster GET request")
+	require.Equal(t, req.TmplClusterKey(), newReq.TmplClusterKey(), "cluster keys should be equal")
 }
