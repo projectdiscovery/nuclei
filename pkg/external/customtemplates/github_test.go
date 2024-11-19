13af7ccd492b86131cd33a10e069809955da341d
Dwi Siswanto <25837540+dwisiswant0@users.noreply.github.com>
2024-11-19 12:38:25 +0700

    fix: data race at `protocolstate`, `contextargs` & some outdated test cases (#5820)
    * test(flow): update outdated test cases
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>
    
    * test(multiproto): update outdated test cases
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>
    
    * feat: fixed failing tests
    
    * fixed data race
    
    * fixed memgaurdian race conditiong
    
    * test(customtemplates): use test repo
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>
    
    * feat(customtemplates): add more `{Clone,Pull}Options`
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>
    
    * feat(customtemplates): validate `{Clone,Pull}Options`
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>
    
    * bugfix: fixed failing integration tests for flow and multi
    
    * chore: either 1 or 2 results in interactsh
    
    ---------
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>
    Co-authored-by: Ice3man <nizamulrana@gmail.com>

--- pkg/external/customtemplates/github_test.go
+++ pkg/external/customtemplates/github_test.go
@@ -22,14 +22,12 @@ func TestDownloadCustomTemplatesFromGitHub(t *testing.T) {
 	config.DefaultConfig.SetTemplatesDir(templatesDirectory)
 
 	options := testutils.DefaultOptions
-	options.GitHubTemplateRepo = []string{"projectdiscovery/nuclei-templates", "ehsandeep/nuclei-templates"}
-	options.GitHubToken = os.Getenv("GITHUB_TOKEN")
+	options.GitHubTemplateRepo = []string{"projectdiscovery/nuclei-templates-test"}
 
 	ctm, err := NewCustomTemplatesManager(options)
 	require.Nil(t, err, "could not create custom templates manager")
 
 	ctm.Download(context.Background())
 
-	require.DirExists(t, filepath.Join(templatesDirectory, "github", "projectdiscovery", "nuclei-templates"), "cloned directory does not exists")
-	require.DirExists(t, filepath.Join(templatesDirectory, "github", "ehsandeep", "nuclei-templates"), "cloned directory does not exists")
+	require.DirExists(t, filepath.Join(templatesDirectory, "github", "projectdiscovery", "nuclei-templates-test"), "cloned directory does not exists")
 }
