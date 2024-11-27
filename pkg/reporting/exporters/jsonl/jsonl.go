aac0780a939eeaa47e3f24749c1c88acbe8a462b
Richard Brown <rbrown@bishopfox.com>
2024-11-27 16:24:36 -0600

    removed trailing comma from the jsonl exporter

--- pkg/reporting/exporters/jsonl/jsonl.go
+++ pkg/reporting/exporters/jsonl/jsonl.go
@@ -86,8 +86,7 @@ func (exporter *Exporter) WriteRows() error {
 			return errors.Wrap(err, "failed to generate row for JSONL report")
 		}
 
-		// Add a trailing newline to the JSON byte array to confirm with the JSONL format
-		obj = append(obj, ',', '\n')
+		obj = append(obj, '\n')
 
 		// Attempt to append the JSON line to file specified in options.JSONLExport
 		if _, err = exporter.outputFile.Write(obj); err != nil {
