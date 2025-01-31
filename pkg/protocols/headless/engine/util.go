5b8505f3fae26fa6820c8f9fd20371de69dde73b
Dwi Siswanto <git@dw1.io>
2025-02-01 03:36:41 +0700

    chore(headless): add nolint directive - `replaceWithValues`
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- pkg/protocols/headless/engine/util.go
+++ pkg/protocols/headless/engine/util.go
@@ -9,6 +9,7 @@ import (
 // replaceWithValues replaces the template markers with the values
 //
 // Deprecated: Not used anymore.
+// nolint: unused
 func replaceWithValues(data string, values map[string]interface{}) string {
 	return fasttemplate.ExecuteStringStd(data, marker.ParenthesisOpen, marker.ParenthesisClose, values)
 }
