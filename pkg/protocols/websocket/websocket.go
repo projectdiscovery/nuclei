f82f1c05e3fac8f0a5c01dee33fc83556bc02091
Shubham Rasal <shubham@projectdiscovery.io>
2025-06-16 22:24:52 +0530

    Use proxy for dns and ssl templates (#6255)
    * Use proxy for dns and ssl templates
    
    - while using template execute level function we need to override custom dialer
    
    * rename overridedialer to customdialer
    
    * Add proxy into hash
    
    - proxy client is shared between non proxy requests
    
    * add dialer into request object
    
    - use request.dialer instead of global variable
    
    * resolve comments
    
    * rename dialer

--- pkg/protocols/websocket/websocket.go
+++ pkg/protocols/websocket/websocket.go
@@ -100,7 +100,9 @@ const (
 func (request *Request) Compile(options *protocols.ExecutorOptions) error {
 	request.options = options
 
-	client, err := networkclientpool.Get(options.Options, &networkclientpool.Configuration{})
+	client, err := networkclientpool.Get(options.Options, &networkclientpool.Configuration{
+		CustomDialer: options.CustomFastdialer,
+	})
 	if err != nil {
 		return errors.Wrap(err, "could not get network client")
 	}
