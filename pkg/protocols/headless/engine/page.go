d2dc19d972b75398c6fa1621c1b66e36bb9ade19
Dwi Siswanto <25837540+dwisiswant0@users.noreply.github.com>
2025-06-24 07:02:18 +0700

    fix(headless): incorrect last navigated URL (#6278)
    * chore(headless): uses `maps.Copy`
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>
    
    * feat(headless): implements update last navigated URL
    
    for `ActionNavigate`, `WaitPageLifecycleEvent`, and
    `WaitStable` based on latest navigation URL.
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>
    
    * Update pkg/protocols/headless/engine/page.go
    
    ---------
    
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- pkg/protocols/headless/engine/page.go
+++ pkg/protocols/headless/engine/page.go
@@ -24,19 +24,20 @@ import (
 
 // Page is a single page in an isolated browser instance
 type Page struct {
-	ctx            *contextargs.Context
-	inputURL       *urlutil.URL
-	options        *Options
-	page           *rod.Page
-	rules          []rule
-	instance       *Instance
-	hijackRouter   *rod.HijackRouter
-	hijackNative   *Hijack
-	mutex          *sync.RWMutex
-	History        []HistoryData
-	InteractshURLs []string
-	payloads       map[string]interface{}
-	variables      map[string]interface{}
+	ctx                *contextargs.Context
+	inputURL           *urlutil.URL
+	options            *Options
+	page               *rod.Page
+	rules              []rule
+	instance           *Instance
+	hijackRouter       *rod.HijackRouter
+	hijackNative       *Hijack
+	mutex              *sync.RWMutex
+	History            []HistoryData
+	InteractshURLs     []string
+	payloads           map[string]interface{}
+	variables          map[string]interface{}
+	lastActionNavigate *Action
 }
 
 // HistoryData contains the page request/response pairs
@@ -272,6 +273,17 @@ func (p *Page) hasModificationRules() bool {
 		}
 	}
 	return false
+}
+
+// updateLastNavigatedURL updates the last navigated URL in the instance's
+// request log.
+func (p *Page) updateLastNavigatedURL() {
+	if p.lastActionNavigate == nil {
+		return
+	}
+
+	templateURL := p.lastActionNavigate.GetArg("url")
+	p.instance.requestLog[templateURL] = p.URL()
 }
 
 func containsModificationActions(actions ...*Action) bool {
