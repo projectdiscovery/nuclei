aa9cc26af5573e5a530c5e8181669bdc2bfd8af1
Mzack9999 <mzack9999@protonmail.com>
2025-07-17 14:18:29 +0200

    debug gh action

--- pkg/external/customtemplates/github_test.go
+++ pkg/external/customtemplates/github_test.go
@@ -2,22 +2,31 @@ package customtemplates
 
 import (
 	"context"
+	"os"
 	"path/filepath"
 	"testing"
 
 	"github.com/projectdiscovery/gologger"
+	"github.com/projectdiscovery/gologger/levels"
 	"github.com/projectdiscovery/nuclei/v3/pkg/catalog/config"
 	"github.com/projectdiscovery/nuclei/v3/pkg/testutils"
-	osutils "github.com/projectdiscovery/utils/os"
 	"github.com/stretchr/testify/require"
 )
 
-func TestDownloadCustomTemplatesFromGitHub(t *testing.T) {
-	if osutils.IsOSX() {
-		t.Skip("skipping on macos due to unknown failure (works locally)")
-	}
+// stdoutWriter adapts os.Stdout to the gologger writer interface
+type stdoutWriter struct{}
 
-	gologger.DefaultLogger.SetWriter(&testutils.NoopWriter{})
+func (w *stdoutWriter) Write(data []byte, level levels.Level) {
+	os.Stdout.Write(data)
+}
+
+func TestDownloadCustomTemplatesFromGitHub(t *testing.T) {
+	// if osutils.IsOSX() {
+	// 	t.Skip("skipping on macos due to unknown failure (works locally)")
+	// }
+
+	gologger.DefaultLogger.SetWriter(&stdoutWriter{})
+	gologger.DefaultLogger.SetMaxLevel(levels.LevelDebug)
 
 	templatesDirectory := t.TempDir()
 	config.DefaultConfig.SetTemplatesDir(templatesDirectory)
@@ -29,5 +38,6 @@ func TestDownloadCustomTemplatesFromGitHub(t *testing.T) {
 	require.Nil(t, err, "could not create custom templates manager")
 
 	ctm.Download(context.Background())
+
 	require.DirExists(t, filepath.Join(templatesDirectory, "github", "projectdiscovery", "nuclei-templates-test"), "cloned directory does not exists")
 }
