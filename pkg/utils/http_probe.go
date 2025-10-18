361f87773067d00c593aad0be56003d6ff7c9782
Matej Smycka <45017046+matejsmycka@users.noreply.github.com>
2025-10-18 14:05:37 +0200

    Http probing optimizations high ports (#6538)
    * feat: Assume HTTP(S) server on high port is HTTP
    
    * feat: enhance http probing tests
    
    * improving issue description
    
    ---------
    
    Co-authored-by: Matej Smycka <smycka@ics.muni.cz>
    Co-authored-by: Mzack9999 <mzack9999@protonmail.com>

--- pkg/utils/http_probe.go
+++ pkg/utils/http_probe.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"net/http"
+	"strconv"
 
 	"github.com/projectdiscovery/httpx/common/httpx"
 	"github.com/projectdiscovery/nuclei/v3/pkg/input/types"
@@ -26,9 +27,16 @@ var httpFirstSchemes = []string{
 
 // determineSchemeOrder for the input
 func determineSchemeOrder(input string) []string {
-	// if input has port that is commonly used for HTTP, return http then https
 	if _, port, err := net.SplitHostPort(input); err == nil {
+		// if input has port that is commonly used for HTTP, return http then https
 		if sliceutil.Contains(commonHttpPorts, port) {
+			return httpFirstSchemes
+		}
+
+		// As of 10/2025 shodan shows that ports > 1024 are more likely to expose HTTP
+		// hence we test first http then https on higher ports
+		// if input has port > 1024, return http then https
+		if port, err := strconv.Atoi(port); err == nil && port > 1024 {
 			return httpFirstSchemes
 		}
 	}
