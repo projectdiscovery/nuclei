2829fd30bb51a2cf9dae9068230bc9dc3ea8e496
Niek den Breeje <n.denbreeje@guardian360.nl>
2025-11-07 12:01:46 +0100

    fix(sdk): configure tmpDir for SDK
    Closes #6595.

--- lib/sdk.go
+++ lib/sdk.go
@@ -5,6 +5,7 @@ import (
 	"bytes"
 	"context"
 	"io"
+	"os"
 	"sync"
 
 	"github.com/projectdiscovery/gologger"
@@ -92,6 +93,9 @@ type NucleiEngine struct {
 
 	// Logger instance for the engine
 	Logger *gologger.Logger
+
+	// General purpose temporary directory
+	tmpDir string
 }
 
 // LoadAllTemplates loads all nuclei template based on given options
@@ -230,6 +234,9 @@ func (e *NucleiEngine) closeInternal() {
 	}
 	if e.httpxClient != nil {
 		_ = e.httpxClient.Close()
+	}
+	if e.tmpDir != "" {
+		_ = os.RemoveAll(e.tmpDir)
 	}
 }
 
