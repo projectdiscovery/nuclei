80321bce4c19233f8e780b411952a080d1fb3d7d
Doğan Can Bakır <dogancanbakir@protonmail.com>
2025-11-25 12:38:08 +0900

    add variable support to extractors

--- pkg/operators/extractors/extract_test.go
+++ pkg/operators/extractors/extract_test.go
@@ -11,10 +11,10 @@ func TestExtractor_ExtractRegex(t *testing.T) {
 	err := e.CompileExtractors()
 	require.Nil(t, err)
 
-	got := e.ExtractRegex("RegEx")
+	got := e.ExtractRegex("RegEx", map[string]interface{}{})
 	require.Equal(t, map[string]struct{}{"RegEx": {}}, got)
 
-	got = e.ExtractRegex("regex")
+	got = e.ExtractRegex("regex", map[string]interface{}{})
 	require.Equal(t, map[string]struct{}{}, got)
 }
 
@@ -70,10 +70,10 @@ func TestExtractor_ExtractJSON(t *testing.T) {
 	err := e.CompileExtractors()
 	require.Nil(t, err)
 
-	got := e.ExtractJSON(`[{"id": 1}]`)
+	got := e.ExtractJSON(`[{"id": 1}]`, map[string]interface{}{})
 	require.Equal(t, map[string]struct{}{"1": {}}, got)
 
-	got = e.ExtractJSON(`{"id": 1}`)
+	got = e.ExtractJSON(`{"id": 1}`, map[string]interface{}{})
 	require.Equal(t, map[string]struct{}{}, got)
 }
 
