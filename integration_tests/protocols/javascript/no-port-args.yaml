projectdiscovery/nuclei

commit f4f2e9f23b51605c38994a7f309411e1c55e0e7e

restoring basic sequential multiport support

pkg/protocols/javascript/js.go
@@ -7,7 +7,6 @@ import (
 	"maps"
 	"net"
 	"strings"
-	"sync"
 	"sync/atomic"
 	"time"
 

commit 537532c40e07c4b0eff82ffe23075e6f433febcf

fix(javascript): restore exec for templates w/o `Port` arg
Restore backwards compat for JavaScript protocol
templates that omit the `Port` argument.
Regression was introduced in f4f2e9f2, which
removed the fallback for empty `Port` in
`(*Request).ExecuteWithResults`, causing templates
without `Port` to be silently skipped.

Now, if no `Port` is specified, the engine
executes the JavaScript block using the target
URL's port.

Fixes #6708.

Signed-off-by: Dwi Siswanto <git@dw1.io>

pkg/protocols/javascript/js.go
@@ -287,6 +287,9 @@ func (request *Request) GetID() string {
 func (request *Request) ExecuteWithResults(target *contextargs.Context, dynamicValues, previous output.InternalEvent, callback protocols.OutputEventCallback) error {
 	// Get default port(s) if specified in template
 	ports := request.getPorts()
+	if len(ports) == 0 {
+		return request.executeWithResults("", target, dynamicValues, previous, callback)
+	}
 
 	var errs []error
 

cmd/integration-test/javascript.go
@@ -18,6 +18,7 @@ var jsTestcases = []TestCaseInfo{
 	{Path: "protocols/javascript/oracle-auth-test.yaml", TestCase: &javascriptOracleAuthTest{}, DisableOn: func() bool { return osutils.IsWindows() || osutils.IsOSX() }},
 	{Path: "protocols/javascript/vnc-pass-brute.yaml", TestCase: &javascriptVncPassBrute{}},
 	{Path: "protocols/javascript/multi-ports.yaml", TestCase: &javascriptMultiPortsSSH{}},
+	{Path: "protocols/javascript/no-port-args.yaml", TestCase: &javascriptNoPortArgs{}},
 }
 
 var (
@@ -173,6 +174,16 @@ type javascriptMultiPortsSSH struct{}
 func (j *javascriptMultiPortsSSH) Execute(filePath string) error {
 	// use scanme.sh as target to ensure we match on the 2nd default port 22
 	results, err := testutils.RunNucleiTemplateAndGetResults(filePath, "scanme.sh", debug)
+	if err != nil {
+		return err
+	}
+	return expectResultsCount(results, 1)
+}
+
+type javascriptNoPortArgs struct{}
+
+func (j *javascriptNoPortArgs) Execute(filePath string) error {
+	results, err := testutils.RunNucleiTemplateAndGetResults(filePath, "yo.dawg", debug)
 	if err != nil {
 		return err
 	}

integration_tests/protocols/javascript/no-port-args.yaml
@@ -1,0 +1,23 @@
+id: javascript-no-port-args
+
+info:
+  name: JavaScript Template Without Port Args
+  author: dwisiswant0
+  severity: info
+  description: |
+    Test backwards compatibility for JavaScript templates without Port in args.
+    Templates should execute even when Port arg is not explicitly specified.
+
+javascript:
+  - code: |
+      // Simple JavaScript code that does not require Port
+      let result = "executed";
+      Export(result);
+
+    args:
+      TestArg: "test-value"
+
+    matchers:
+      - type: dsl
+        dsl:
+          - success == true