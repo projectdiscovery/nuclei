projectdiscovery/nuclei

commit c33a9846d10c2b72f756e0b1b143805a61898f3e

Remove singletons from Nuclei engine (continuation of #6210) (#6296)
* introducing execution id

* wip

* .

* adding separate execution context id

* lint

* vet

* fixing pg dialers

* test ignore

* fixing loader FD limit

* test

* fd fix

* wip: remove CloseProcesses() from dev merge

* wip: fix merge issue

* protocolstate: stop memguarding on last dialer delete

* avoid data race in dialers.RawHTTPClient

* use shared logger and avoid race conditions

* use shared logger and avoid race conditions

* go mod

* patch executionId into compiled template cache

* clean up comment in Parse

* go mod update

* bump echarts

* address merge issues

* fix use of gologger

* switch cmd/nuclei to options.Logger

* address merge issues with go.mod

* go vet: address copy of lock with new Copy function

* fixing tests

* disable speed control

* fix nil ExecuterOptions

* removing deprecated code

* fixing result print

* default logger

* cli default logger

* filter warning from results

* fix performance test

* hardcoding path

* disable upload

* refactor(runner): uses `Warning` instead of `Print` for `pdcpUploadErrMsg`

Signed-off-by: Dwi Siswanto <git@dw1.io>

* Revert "disable upload"

This reverts commit 114fbe6663361bf41cf8b2645fd2d57083d53682.

* Revert "hardcoding path"

This reverts commit cf12ca800e0a0e974bd9fd4826a24e51547f7c00.

---------

Signed-off-by: Dwi Siswanto <git@dw1.io>
Co-authored-by: Mzack9999 <mzack9999@protonmail.com>
Co-authored-by: Dwi Siswanto <git@dw1.io>
Co-authored-by: Dwi Siswanto <25837540+dwisiswant0@users.noreply.github.com>

pkg/js/libs/mysql/memo.mysql.go
@@ -8,11 +8,11 @@ import (
 	"github.com/projectdiscovery/nuclei/v3/pkg/protocols/common/protocolstate"
 )
 
-func memoizedisMySQL(host string, port int) (bool, error) {
+func memoizedisMySQL(executionId string, host string, port int) (bool, error) {
 	hash := "isMySQL" + ":" + fmt.Sprint(host) + ":" + fmt.Sprint(port)
 
 	v, err, _ := protocolstate.Memoizer.Do(hash, func() (interface{}, error) {
-		return isMySQL(host, port)
+		return isMySQL(executionId, host, port)
 	})
 	if err != nil {
 		return false, err
@@ -24,11 +24,11 @@ func memoizedisMySQL(host string, port int) (bool, error) {
 	return false, errors.New("could not convert cached result")
 }
 
-func memoizedfingerprintMySQL(host string, port int) (MySQLInfo, error) {
+func memoizedfingerprintMySQL(executionId string, host string, port int) (MySQLInfo, error) {
 	hash := "fingerprintMySQL" + ":" + fmt.Sprint(host) + ":" + fmt.Sprint(port)
 
 	v, err, _ := protocolstate.Memoizer.Do(hash, func() (interface{}, error) {
-		return fingerprintMySQL(host, port)
+		return fingerprintMySQL(executionId, host, port)
 	})
 	if err != nil {
 		return MySQLInfo{}, err

pkg/js/libs/mysql/mysql.go
@@ -35,24 +35,30 @@ type (
 // const mysql = require('nuclei/mysql');
 // const isMySQL = mysql.IsMySQL('acme.com', 3306);
 // ```
-func (c *MySQLClient) IsMySQL(host string, port int) (bool, error) {
+func (c *MySQLClient) IsMySQL(ctx context.Context, host string, port int) (bool, error) {
+	executionId := ctx.Value("executionId").(string)
 	// todo: why this is exposed? Service fingerprint should be automatic
-	return memoizedisMySQL(host, port)
+	return memoizedisMySQL(executionId, host, port)
 }
 
 // @memo
-func isMySQL(host string, port int) (bool, error) {
-	if !protocolstate.IsHostAllowed(host) {
+func isMySQL(executionId string, host string, port int) (bool, error) {
+	if !protocolstate.IsHostAllowed(executionId, host) {
 		// host is not valid according to network policy
 		return false, protocolstate.ErrHostDenied.Msgf(host)
 	}
-	conn, err := protocolstate.Dialer.Dial(context.TODO(), "tcp", net.JoinHostPort(host, fmt.Sprintf("%d", port)))
+	dialer := protocolstate.GetDialersWithId(executionId)
+	if dialer == nil {
+		return false, fmt.Errorf("dialers not initialized for %s", executionId)
+	}
+
+	conn, err := dialer.Fastdialer.Dial(context.TODO(), "tcp", net.JoinHostPort(host, fmt.Sprintf("%d", port)))
 	if err != nil {
 		return false, err
 	}
 	defer func() {
-         _ = conn.Close()
-       }()
+		_ = conn.Close()
+	}()
 
 	plugin := &mysqlplugin.MYSQLPlugin{}
 	service, err := plugin.Run(conn, 5*time.Second, plugins.Target{Host: host})
@@ -75,14 +81,15 @@ func isMySQL(host string, port int) (bool, error) {
 // const client = new mysql.MySQLClient;
 // const connected = client.Connect('acme.com', 3306, 'username', 'password');
 // ```
-func (c *MySQLClient) Connect(host string, port int, username, password string) (bool, error) {
-	if !protocolstate.IsHostAllowed(host) {
+func (c *MySQLClient) Connect(ctx context.Context, host string, port int, username, password string) (bool, error) {
+	executionId := ctx.Value("executionId").(string)
+	if !protocolstate.IsHostAllowed(executionId, host) {
 		// host is not valid according to network policy
 		return false, protocolstate.ErrHostDenied.Msgf(host)
 	}
 
 	// executing queries implies the remote mysql service
-	ok, err := c.IsMySQL(host, port)
+	ok, err := c.IsMySQL(ctx, host, port)
 	if err != nil {
 		return false, err
 	}
@@ -127,24 +134,30 @@ type (
 // const info = mysql.FingerprintMySQL('acme.com', 3306);
 // log(to_json(info));
 // ```
-func (c *MySQLClient) FingerprintMySQL(host string, port int) (MySQLInfo, error) {
-	return memoizedfingerprintMySQL(host, port)
+func (c *MySQLClient) FingerprintMySQL(ctx context.Context, host string, port int) (MySQLInfo, error) {
+	executionId := ctx.Value("executionId").(string)
+	return memoizedfingerprintMySQL(executionId, host, port)
 }
 
 // @memo
-func fingerprintMySQL(host string, port int) (MySQLInfo, error) {
+func fingerprintMySQL(executionId string, host string, port int) (MySQLInfo, error) {
 	info := MySQLInfo{}
-	if !protocolstate.IsHostAllowed(host) {
+	if !protocolstate.IsHostAllowed(executionId, host) {
 		// host is not valid according to network policy
 		return info, protocolstate.ErrHostDenied.Msgf(host)
 	}
-	conn, err := protocolstate.Dialer.Dial(context.TODO(), "tcp", net.JoinHostPort(host, fmt.Sprintf("%d", port)))
+	dialer := protocolstate.GetDialersWithId(executionId)
+	if dialer == nil {
+		return MySQLInfo{}, fmt.Errorf("dialers not initialized for %s", executionId)
+	}
+
+	conn, err := dialer.Fastdialer.Dial(context.TODO(), "tcp", net.JoinHostPort(host, fmt.Sprintf("%d", port)))
 	if err != nil {
 		return info, err
 	}
 	defer func() {
-         _ = conn.Close()
-       }()
+		_ = conn.Close()
+	}()
 
 	plugin := &mysqlplugin.MYSQLPlugin{}
 	service, err := plugin.Run(conn, 5*time.Second, plugins.Target{Host: host})
@@ -192,14 +205,15 @@ func (c *MySQLClient) ConnectWithDSN(dsn string) (bool, error) {
 // const result = mysql.ExecuteQueryWithOpts(options, 'SELECT * FROM users');
 // log(to_json(result));
 // ```
-func (c *MySQLClient) ExecuteQueryWithOpts(opts MySQLOptions, query string) (*utils.SQLResult, error) {
-	if !protocolstate.IsHostAllowed(opts.Host) {
+func (c *MySQLClient) ExecuteQueryWithOpts(ctx context.Context, opts MySQLOptions, query string) (*utils.SQLResult, error) {
+	executionId := ctx.Value("executionId").(string)
+	if !protocolstate.IsHostAllowed(executionId, opts.Host) {
 		// host is not valid according to network policy
 		return nil, protocolstate.ErrHostDenied.Msgf(opts.Host)
 	}
 
 	// executing queries implies the remote mysql service
-	ok, err := c.IsMySQL(opts.Host, opts.Port)
+	ok, err := c.IsMySQL(ctx, opts.Host, opts.Port)
 	if err != nil {
 		return nil, err
 	}
@@ -217,8 +231,8 @@ func (c *MySQLClient) ExecuteQueryWithOpts(opts MySQLOptions, query string) (*ut
 		return nil, err
 	}
 	defer func() {
-         _ = db.Close()
-       }()
+		_ = db.Close()
+	}()
 	db.SetMaxOpenConns(1)
 	db.SetMaxIdleConns(0)
 
@@ -246,9 +260,9 @@ func (c *MySQLClient) ExecuteQueryWithOpts(opts MySQLOptions, query string) (*ut
 // const result = mysql.ExecuteQuery('acme.com', 3306, 'username', 'password', 'SELECT * FROM users');
 // log(to_json(result));
 // ```
-func (c *MySQLClient) ExecuteQuery(host string, port int, username, password, query string) (*utils.SQLResult, error) {
+func (c *MySQLClient) ExecuteQuery(ctx context.Context, host string, port int, username, password, query string) (*utils.SQLResult, error) {
 	// executing queries implies the remote mysql service
-	ok, err := c.IsMySQL(host, port)
+	ok, err := c.IsMySQL(ctx, host, port)
 	if err != nil {
 		return nil, err
 	}
@@ -256,7 +270,7 @@ func (c *MySQLClient) ExecuteQuery(host string, port int, username, password, qu
 		return nil, fmt.Errorf("not a mysql service")
 	}
 
-	return c.ExecuteQueryWithOpts(MySQLOptions{
+	return c.ExecuteQueryWithOpts(ctx, MySQLOptions{
 		Host:     host,
 		Port:     port,
 		Protocol: "tcp",
@@ -273,8 +287,8 @@ func (c *MySQLClient) ExecuteQuery(host string, port int, username, password, qu
 // const result = mysql.ExecuteQueryOnDB('acme.com', 3306, 'username', 'password', 'dbname', 'SELECT * FROM users');
 // log(to_json(result));
 // ```
-func (c *MySQLClient) ExecuteQueryOnDB(host string, port int, username, password, dbname, query string) (*utils.SQLResult, error) {
-	return c.ExecuteQueryWithOpts(MySQLOptions{
+func (c *MySQLClient) ExecuteQueryOnDB(ctx context.Context, host string, port int, username, password, dbname, query string) (*utils.SQLResult, error) {
+	return c.ExecuteQueryWithOpts(ctx, MySQLOptions{
 		Host:     host,
 		Port:     port,
 		Protocol: "tcp",

pkg/js/libs/mysql/mysql_private.go
@@ -78,8 +78,8 @@ func connectWithDSN(dsn string) (bool, error) {
 		return false, err
 	}
 	defer func() {
-         _ = db.Close()
-       }()
+		_ = db.Close()
+	}()
 	db.SetMaxOpenConns(1)
 	db.SetMaxIdleConns(0)
 

pkg/protocols/common/protocolstate/state.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net"
 	"net/url"
-	"sync"
 
 	"github.com/go-sql-driver/mysql"
 	"github.com/pkg/errors"
@@ -16,32 +15,54 @@ import (
 	"github.com/projectdiscovery/networkpolicy"
 	"github.com/projectdiscovery/nuclei/v3/pkg/types"
 	"github.com/projectdiscovery/nuclei/v3/pkg/utils/expand"
+	"github.com/projectdiscovery/retryablehttp-go"
+	mapsutil "github.com/projectdiscovery/utils/maps"
 )
 
-// Dialer is a shared fastdialer instance for host DNS resolution
 var (
-	muDialer sync.RWMutex
-	Dialer   *fastdialer.Dialer
+	dialers *mapsutil.SyncLockMap[string, *Dialers]
 )
 
-func GetDialer() *fastdialer.Dialer {
-	muDialer.RLock()
-	defer muDialer.RUnlock()
-
-	return Dialer
+func init() {
+	dialers = mapsutil.NewSyncLockMap[string, *Dialers]()
 }
 
-func ShouldInit() bool {
-	return Dialer == nil
+func GetDialers(ctx context.Context) *Dialers {
+	executionContext := GetExecutionContext(ctx)
+	dialers, ok := dialers.Get(executionContext.ExecutionID)
+	if !ok {
+		return nil
+	}
+	return dialers
 }
 
-// Init creates the Dialer instance based on user configuration
+func GetDialersWithId(id string) *Dialers {
+	dialers, ok := dialers.Get(id)
+	if !ok {
+		return nil
+	}
+	return dialers
+}
+
+func ShouldInit(id string) bool {
+	dialer, ok := dialers.Get(id)
+	if !ok {
+		return true
+	}
+	return dialer == nil
+}
+
+// Init creates the Dialers instance based on user configuration
 func Init(options *types.Options) error {
-	if Dialer != nil {
+	if GetDialersWithId(options.ExecutionId) != nil {
 		return nil
 	}
 
-	lfaAllowed = options.AllowLocalFileAccess
+	return initDialers(options)
+}
+
+// initDialers is the internal implementation of Init
+func initDialers(options *types.Options) error {
 	opts := fastdialer.DefaultOptions
 	opts.DialerTimeout = options.GetTimeouts().DialTimeout
 	if options.DialerKeepAlive > 0 {
@@ -66,8 +87,6 @@ func Init(options *types.Options) error {
 		DenyList: expandedDenyList,
 	}
 	opts.WithNetworkPolicyOptions = npOptions
-	NetworkPolicy, _ = networkpolicy.New(*npOptions)
-	InitHeadless(options.AllowLocalFileAccess, NetworkPolicy)
 
 	switch {
 	case options.SourceIP != "" && options.Interface != "":
@@ -152,7 +171,17 @@ func Init(options *types.Options) error {
 	if err != nil {
 		return errors.Wrap(err, "could not create dialer")
 	}
-	Dialer = dialer
+
+	networkPolicy, _ := networkpolicy.New(*npOptions)
+
+	dialersInstance := &Dialers{
+		Fastdialer:             dialer,
+		NetworkPolicy:          networkPolicy,
+		HTTPClientPool:         mapsutil.NewSyncLockMap[string, *retryablehttp.Client](),
+		LocalFileAccessAllowed: options.AllowLocalFileAccess,
+	}
+
+	_ = dialers.Set(options.ExecutionId, dialersInstance)
 
 	// Set a custom dialer for the "nucleitcp" protocol.  This is just plain TCP, but it's registered
 	// with a different name so that we do not clobber the "tcp" dialer in the event that nuclei is
@@ -164,10 +193,16 @@ func Init(options *types.Options) error {
 			addr += ":3306"
 		}
 
-		return Dialer.Dial(ctx, "tcp", addr)
+		executionId := ctx.Value("executionId").(string)
+		dialer := GetDialersWithId(executionId)
+		return dialer.Fastdialer.Dial(ctx, "tcp", addr)
 	})
 
 	StartActiveMemGuardian(context.Background())
+
+	// TODO: this should be tied to executionID
+	// overidde global settings with latest options
+	LfaAllowed = options.AllowLocalFileAccess
 
 	return nil
 }
@@ -226,13 +261,19 @@ func interfaceAddresses(interfaceName string) ([]net.Addr, error) {
 }
 
 // Close closes the global shared fastdialer
-func Close() {
-	muDialer.Lock()
-	defer muDialer.Unlock()
-
-	if Dialer != nil {
-		Dialer.Close()
-		Dialer = nil
+func Close(executionId string) {
+	dialersInstance, ok := dialers.Get(executionId)
+	if !ok {
+		return
 	}
-	StopActiveMemGuardian()
+
+	if dialersInstance != nil {
+		dialersInstance.Fastdialer.Close()
+	}
+
+	dialers.Delete(executionId)
+
+	if dialers.IsEmpty() {
+		StopActiveMemGuardian()
+	}
 }

pkg/js/generated/go/libmysql/mysql.go
@@ -3,7 +3,7 @@ package mysql
 import (
 	lib_mysql "github.com/projectdiscovery/nuclei/v3/pkg/js/libs/mysql"
 
-	"github.com/dop251/goja"
+	"github.com/Mzack9999/goja"
 	"github.com/projectdiscovery/nuclei/v3/pkg/js/gojs"
 )
 

commit 0eb87c2621da09b45147a7d2974c6b589d5ce20d

fix(js): mysql panic due to missing `executionId` in ctx
The `connectWithDSN` func used `db.Exec()` which
implicitly uses `context.Background()`[1]. This
caused the registered "nucleitcp" dialer
callback to receive a ctx missing the
`executionId`, leading to a panic during type
assertion.

Refactor `connectWithDSN` to accept `executionId`
explicitly and use it to create a `context` for
`db.PingContext()` (yeah, instead of `db.Exec()`).
And, add a defensive check in the dialer callback
to handle nil values gracefully.

Fixes #6733 regression introduced in #6296.

[1]: "Exec uses `context.Background` internally" -
     https://pkg.go.dev/database/sql#DB.Exec.

Signed-off-by: Dwi Siswanto <git@dw1.io>

pkg/js/libs/mysql/memo.mysql.go
@@ -9,7 +9,7 @@ import (
 )
 
 func memoizedisMySQL(executionId string, host string, port int) (bool, error) {
-	hash := "isMySQL" + ":" + fmt.Sprint(host) + ":" + fmt.Sprint(port)
+	hash := "isMySQL" + ":" + fmt.Sprint(executionId) + ":" + fmt.Sprint(host) + ":" + fmt.Sprint(port)
 
 	v, err, _ := protocolstate.Memoizer.Do(hash, func() (interface{}, error) {
 		return isMySQL(executionId, host, port)
@@ -25,7 +25,7 @@ func memoizedisMySQL(executionId string, host string, port int) (bool, error) {
 }
 
 func memoizedfingerprintMySQL(executionId string, host string, port int) (MySQLInfo, error) {
-	hash := "fingerprintMySQL" + ":" + fmt.Sprint(host) + ":" + fmt.Sprint(port)
+	hash := "fingerprintMySQL" + ":" + fmt.Sprint(executionId) + ":" + fmt.Sprint(host) + ":" + fmt.Sprint(port)
 
 	v, err, _ := protocolstate.Memoizer.Do(hash, func() (interface{}, error) {
 		return fingerprintMySQL(executionId, host, port)

pkg/js/libs/mysql/memo.mysql_private.go
@@ -8,11 +8,11 @@ import (
 	"github.com/projectdiscovery/nuclei/v3/pkg/protocols/common/protocolstate"
 )
 
-func memoizedconnectWithDSN(dsn string) (bool, error) {
-	hash := "connectWithDSN" + ":" + fmt.Sprint(dsn)
+func memoizedconnectWithDSN(executionId string, dsn string) (bool, error) {
+	hash := "connectWithDSN" + ":" + fmt.Sprint(executionId) + ":" + fmt.Sprint(dsn)
 
 	v, err, _ := protocolstate.Memoizer.Do(hash, func() (interface{}, error) {
-		return connectWithDSN(dsn)
+		return connectWithDSN(executionId, dsn)
 	})
 	if err != nil {
 		return false, err

pkg/js/libs/mysql/mysql.go
@@ -108,7 +108,7 @@ func (c *MySQLClient) Connect(ctx context.Context, host string, port int, userna
 	if err != nil {
 		return false, err
 	}
-	return connectWithDSN(dsn)
+	return connectWithDSN(executionId, dsn)
 }
 
 type (
@@ -190,8 +190,9 @@ func fingerprintMySQL(executionId string, host string, port int) (MySQLInfo, err
 // const client = new mysql.MySQLClient;
 // const connected = client.ConnectWithDSN('username:password@tcp(acme.com:3306)/');
 // ```
-func (c *MySQLClient) ConnectWithDSN(dsn string) (bool, error) {
-	return memoizedconnectWithDSN(dsn)
+func (c *MySQLClient) ConnectWithDSN(ctx context.Context, dsn string) (bool, error) {
+	executionId := ctx.Value("executionId").(string)
+	return memoizedconnectWithDSN(executionId, dsn)
 }
 
 // ExecuteQueryWithOpts connects to Mysql database using given credentials

pkg/js/libs/mysql/mysql_private.go
@@ -1,6 +1,7 @@
 package mysql
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"net"
@@ -72,7 +73,7 @@ func BuildDSN(opts MySQLOptions) (string, error) {
 }
 
 // @memo
-func connectWithDSN(dsn string) (bool, error) {
+func connectWithDSN(executionId string, dsn string) (bool, error) {
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return false, err
@@ -83,7 +84,8 @@ func connectWithDSN(dsn string) (bool, error) {
 	db.SetMaxOpenConns(1)
 	db.SetMaxIdleConns(0)
 
-	_, err = db.Exec("select 1")
+	ctx := context.WithValue(context.Background(), "executionId", executionId)
+	err = db.PingContext(ctx)
 	if err != nil {
 		return false, err
 	}

pkg/protocols/common/protocolstate/state.go
@@ -200,8 +200,14 @@ func initDialers(options *types.Options) error {
 			addr += ":3306"
 		}
 
-		executionId := ctx.Value("executionId").(string)
+		var executionId string
+		if val := ctx.Value("executionId"); val != nil {
+			executionId = val.(string)
+		}
 		dialer := GetDialersWithId(executionId)
+		if dialer == nil {
+			return nil, fmt.Errorf("dialers not initialized for %s", executionId)
+		}
 		return dialer.Fastdialer.Dial(ctx, "tcp", addr)
 	})
 