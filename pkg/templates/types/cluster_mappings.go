842651c1b647205a137ca76e814abefb582840cc
Yaron Niddam <yaron.niddam@wiz.io>
2026-01-23 19:04:25 +0200

    fix

---
+++ pkg/templates/types/cluster_mappings.go
@@ -0,0 +1,26 @@
+package types
+
+// ClusterMappingsMap wraps cluster ID to template IDs mapping
+type ClusterMappingsMap struct {
+	Map map[string][]string
+}
+
+// NewClusterMappingsMap creates a new ClusterMappingsMap from an existing map
+func NewClusterMappingsMap(m map[string][]string) *ClusterMappingsMap {
+	return &ClusterMappingsMap{Map: m}
+}
+
+// Get returns the template IDs for a given cluster ID
+func (c *ClusterMappingsMap) Get(clusterID string) ([]string, bool) {
+	v, ok := c.Map[clusterID]
+	return v, ok
+}
+
+// GetAll returns a copy of the entire map
+func (c *ClusterMappingsMap) GetAll() map[string][]string {
+	result := make(map[string][]string, len(c.Map))
+	for k, v := range c.Map {
+		result[k] = append([]string{}, v...)
+	}
+	return result
+}
