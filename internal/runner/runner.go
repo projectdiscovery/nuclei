0a4a829113e03e2b50990031cb41a5696a3e367a
NOVA Creative <nova@creative.agency>
2026-03-14 17:43:57 +0300

    fix(auth): prefetch secrets when secret files are provided

--- internal/runner/runner.go
+++ internal/runner/runner.go
@@ -688,7 +688,7 @@ func (r *Runner) RunEnumeration() error {
 	r.displayExecutionInfo(store)
 
 	// prefetch secrets if enabled
-	if executorOpts.AuthProvider != nil && r.options.PreFetchSecrets {
+	if executorOpts.AuthProvider != nil && (r.options.PreFetchSecrets || len(r.options.SecretsFile) > 0) {
 		r.Logger.Info().Msgf("Pre-fetching secrets from authprovider[s]")
 		if err := executorOpts.AuthProvider.PreFetchSecrets(); err != nil {
 			return errors.Wrap(err, "could not pre-fetch secrets")
