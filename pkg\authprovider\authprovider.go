6bb1f4450c3b1781ca1c9a98b7189091e2e1057c
obsofficer-ctrl <obsofficer@gmail.com>
2026-03-19 17:00:14 +0400

    Update pkg\authprovider\authprovider.go

--- pkg\authprovider\authprovider.go
+++ pkg\authprovider\authprovider.go
@@ -9,12 +9,38 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
+// AuthType is the type of auth provider
+type AuthType string
+
+const (
+	BasicAuth  AuthType = "BasicAuth"
+	BearerAuth AuthType = "BearerTokenAuth"
+	CookieAuth AuthType = "CookiesAuth"
+	HeaderAuth AuthType = "HeadersAuth"
+	QueryAuth  AuthType = "QueryAuth"
+	DynamicAuth AuthType = "DynamicAuth"
+)
+
+// AuthStrategy is the interface for auth strategies
+type AuthStrategy interface {
+	// Apply applies the auth strategy to the request
+	Apply(args *AuthStrategyArgs) error
+	// GetType returns the type of auth strategy
+	GetType() AuthType
+}
+
+// AuthStrategyArgs contains the arguments for an auth strategy
+type AuthStrategyArgs struct {
+	// Request is the request to apply auth to
+	Request interface{}
+}
+
 // AuthProvider is a provider for authentication credentials
 type AuthProvider interface {
-	// LookupURLX looks up credentials for a given URL and returns AuthStrategy
+	// LookupURLX looks up credentials for a given URL
 	LookupURLX(url string) AuthStrategy
 	// GetTemplatePaths returns all template paths for auth
 	GetTemplatePaths() []string
-	// PreFetchSecrets pre-fetches all secrets from template-based auth providers
+	// PreFetchSecrets pre-fetches all secrets
 	PreFetchSecrets() error
 }
