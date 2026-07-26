1f39f73e9341abf6601d3f8c015dbd9a552291d3
Mzack9999 <mzack9999@protonmail.com>
2026-07-26 19:51:32 +0400

    extend js smb for #4707 (#7565)
    * extend smb
    
    * enum services
    
    * unify session
    
    * bridge files
    
    * document matrix
    
    * enum processes
    
    * test session
    
    * test enums
    
    * test bridge
    
    * fix ci
    
    * address review
    
    * fix lint

--- go.mod
+++ go.mod
@@ -94,7 +94,6 @@ require (
 	github.com/projectdiscovery/dsl v0.8.20
 	github.com/projectdiscovery/fasttemplate v0.0.2
 	github.com/projectdiscovery/gcache v0.0.0-20241015120333-12546c6e3f4c
-	github.com/projectdiscovery/go-smb2 v0.0.0-20240129202741-052cc450c6cb
 	github.com/projectdiscovery/goflags v0.1.75
 	github.com/projectdiscovery/goja v0.0.0-20260618133720-acb73e419534
 	github.com/projectdiscovery/goja_nodejs v0.0.0-20260618132410-8519f75f703d
--- pkg/js/libs/smb/memo.smb.go
+++ pkg/js/libs/smb/memo.smb.go
@@ -4,11 +4,9 @@ package smb
 import (
 	"context"
 	"errors"
-
 	"fmt"
 
 	"github.com/projectdiscovery/nuclei/v3/pkg/protocols/common/protocolstate"
-
 	"github.com/zmap/zgrab2/lib/smb/smb"
 )
 
@@ -29,7 +27,7 @@ func memoizedconnectSMBInfoMode(ctx context.Context, executionId string, host st
 }
 
 func memoizedlistShares(ctx context.Context, executionId string, host string, port int, user string, password string) ([]string, error) {
-	hash := "listShares" + ":" + fmt.Sprint(executionId) + ":" + fmt.Sprint(host) + ":" + fmt.Sprint(port) + ":" + fmt.Sprint(user) + ":" + fmt.Sprint(password)
+	hash := smbMemoKey("listShares", executionId, host, fmt.Sprint(port), user, password)
 
 	v, err, _ := protocolstate.Memoizer.Do(hash, func() (interface{}, error) {
 		return listShares(ctx, executionId, host, port, user, password)
@@ -42,4 +40,52 @@ func memoizedlistShares(ctx context.Context, executionId string, host string, po
 	}
 
 	return []string{}, errors.New("could not convert cached result")
+}
+
+func memoizedlistDir(ctx context.Context, executionId string, host string, port int, user string, password string, share string, dir string) ([]ShareEntry, error) {
+	hash := smbMemoKey("listDir", executionId, host, fmt.Sprint(port), user, password, share, dir)
+
+	v, err, _ := protocolstate.Memoizer.Do(hash, func() (interface{}, error) {
+		return listDir(ctx, executionId, host, port, user, password, share, dir)
+	})
+	if err != nil {
+		return []ShareEntry{}, err
+	}
+	if value, ok := v.([]ShareEntry); ok {
+		return value, nil
+	}
+
+	return []ShareEntry{}, errors.New("could not convert cached result")
+}
+
+func memoizedreadFile(ctx context.Context, executionId string, host string, port int, user string, password string, share string, filePath string) (string, error) {
+	hash := smbMemoKey("readFile", executionId, host, fmt.Sprint(port), user, password, share, filePath)
+
+	v, err, _ := protocolstate.Memoizer.Do(hash, func() (interface{}, error) {
+		return readFile(ctx, executionId, host, port, user, password, share, filePath)
+	})
+	if err != nil {
+		return "", err
+	}
+	if value, ok := v.(string); ok {
+		return value, nil
+	}
+
+	return "", errors.New("could not convert cached result")
+}
+
+func memoizedlistTree(ctx context.Context, executionId string, host string, port int, user string, password string, share string, dir string) ([]ShareEntry, error) {
+	hash := smbMemoKey("listTree", executionId, host, fmt.Sprint(port), user, password, share, dir)
+
+	v, err, _ := protocolstate.Memoizer.Do(hash, func() (interface{}, error) {
+		return listTree(ctx, executionId, host, port, user, password, share, dir)
+	})
+	if err != nil {
+		return []ShareEntry{}, err
+	}
+	if value, ok := v.([]ShareEntry); ok {
+		return value, nil
+	}
+
+	return []ShareEntry{}, errors.New("could not convert cached result")
 }
